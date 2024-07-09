0702228f78fcbc1223669519b44b0aa7c1e8de7a
devwqc <devwqc@gmail.com>
2024-07-09 11:55:53 +0900

    [섹션2] 02. 보이는 학생

--- 02. 1, 2차원 배열 탐색/02. 보이는 학생/정봉찬/index.js
+++ 02. 1, 2차원 배열 탐색/02. 보이는 학생/정봉찬/index.js
@@ -1,5 +1,29 @@
+/*
+✅문제 제목: 보이는 학생
+
+✅문제 유형: 배열 탐색
+
+✅문제 풀이 날짜: 2024-07-09
+
+💡문제 분석 요약
+  - 앞선 index들의 값보다 큰 값일 때 count를 증가한다.
+
+💡알고리즘 설계
+  - 반복문을 돌면서 max 값을 저장해서 비교한다.
+*/
+
 function solution(arr) {
   let answer = 1;
+
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= max) {
+      continue;
+    }
+    max = arr[i];
+    answer++;
+  }
 
   return answer;
 }
