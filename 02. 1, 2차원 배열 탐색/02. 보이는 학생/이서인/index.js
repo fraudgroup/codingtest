99f74dc0291478c22b8e4cc44effef5cc70952b4
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-09 12:35:34 +0900

    feat: 보이는 학생 풀이

--- 02. 1, 2차원 배열 탐색/02. 보이는 학생/이서인/index.js
+++ 02. 1, 2차원 배열 탐색/02. 보이는 학생/이서인/index.js
@@ -1,6 +1,9 @@
 function solution(arr) {
-  return arr.sort((a, b) => a - b);
+  return arr.filter((el, index) => {
+    if (index === 0) return true;
+    return el > arr[index - 1] && el > Math.max(...arr.slice(0, index));
+  });
 }
 
 let arr = [130, 135, 148, 140, 145, 150, 150, 153];
-console.log(solution(arr));
+console.log(solution(arr).length);
