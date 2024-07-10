da7d938a2008263e21459c9ebb2cc799107979e6
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-10 15:40:45 +0900

    refactor: sortArray 변수 제거

--- 02. 1, 2차원 배열 탐색/05. 등수구하기/윤아영/index.js
+++ 02. 1, 2차원 배열 탐색/05. 등수구하기/윤아영/index.js
@@ -1,7 +1,7 @@
 function solution(arr) {
-  const sortArray = [...arr].sort((a, b) => b - a);
   const sortMap = new Map(
-    sortArray
+    [...arr]
+      .sort((a, b) => b - a)
       .map((v, i) => [v, i + 1])
       .filter((v, i, array) => v[0] !== array[i - 1]?.[0])
   );
