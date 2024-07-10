61ffcd65c16497c1cb426d503b157df5bb4b4b95
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-10 15:31:38 +0900

    feat: 중복되는 점수가 있는 경우 추가

--- 02. 1, 2차원 배열 탐색/05. 등수구하기/윤아영/index.js
+++ 02. 1, 2차원 배열 탐색/05. 등수구하기/윤아영/index.js
@@ -1,8 +1,12 @@
 function solution(arr) {
   const sortArray = [...arr].sort((a, b) => b - a);
-  const sortMap = new Map(sortArray.map((v, i) => [v, i + 1]));
+  const sortMap = new Map(
+    sortArray
+      .map((v, i) => [v, i + 1])
+      .filter((v, i, array) => v[0] !== array[i - 1]?.[0])
+  );
 
-  return arr.map(number => sortMap.get(number));
+  return arr.map((number) => sortMap.get(number));
 }
 
 let arr = [87, 89, 92, 100, 76];
