f07c784c10fc411a95e87e9edd9abb987f7e4bb8
codingaring <sksms2104@naver.com>
2024-07-11 16:35:01 +0900

    2-1 큰 수 출력하기

--- 02. 1, 2차원 배열 탐색/01. 큰 수 출력하기/김유경/index.js
+++ 02. 1, 2차원 배열 탐색/01. 큰 수 출력하기/김유경/index.js
@@ -1,6 +1,13 @@
 function solution(arr) {
   let answer = [];
 
+  answer.push(arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      answer.push(arr[i]);
+    }
+  }
   return answer;
 }
 
