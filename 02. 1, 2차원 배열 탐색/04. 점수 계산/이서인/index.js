318039613c9126a9f952987c940749ce2297d37b
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-11 10:44:48 +0900

    feat: 2-4. 점수 계산 풀이

--- 02. 1, 2차원 배열 탐색/04. 점수 계산/이서인/index.js
+++ 02. 1, 2차원 배열 탐색/04. 점수 계산/이서인/index.js
@@ -1,7 +1,16 @@
 function solution(arr) {
-  let answer = 0;
+  let count = 0;
+  let sum = 0;
 
-  return answer;
+  arr.forEach((el, index) => {
+    if (el === 1) {
+      if (index === 0 || arr[index - 1] === 0) count = 1;
+      if (arr[index - 1] === 1) count++;
+      sum += count;
+    }
+  });
+
+  return sum;
 }
 
 let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
