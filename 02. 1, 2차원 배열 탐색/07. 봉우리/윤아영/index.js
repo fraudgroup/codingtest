356dae1bf8b2240cfa8481fd70e23a56874f9f68
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-13 00:03:36 +0900

    feat: 봉우리 풀이

--- 02. 1, 2차원 배열 탐색/07. 봉우리/윤아영/index.js
+++ 02. 1, 2차원 배열 탐색/07. 봉우리/윤아영/index.js
@@ -1,6 +1,17 @@
 function solution(arr) {
   let answer = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] < arr[i - 1]?.[j]) continue;
+      if (arr[i][j] < arr[i + 1]?.[j]) continue;
+      if (arr[i][j] < arr[i]?.[j - 1]) continue;
+      if (arr[i][j] < arr[i]?.[j + 1]) continue;
+
+      answer++;
+    }
+  }
+
   return answer;
 }
 
