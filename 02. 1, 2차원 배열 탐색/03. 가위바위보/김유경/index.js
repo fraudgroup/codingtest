bff5a6fda8feb17a97e27494455b848420b3bf4c
codingaring <sksms2104@naver.com>
2024-07-12 17:54:00 +0900

    2-3 가위 바위 보

--- 02. 1, 2차원 배열 탐색/03. 가위바위보/김유경/index.js
+++ 02. 1, 2차원 배열 탐색/03. 가위바위보/김유경/index.js
@@ -1,5 +1,38 @@
 function solution(a, b) {
-  let answer = '';
+  let answer = "";
+
+  function checkVictory(a, b) {
+    if (a === b) {
+      return "D";
+    }
+    switch (a) {
+      case 1: {
+        if (b === 2) {
+          return "B";
+        } else if (b === 3) {
+          return "A";
+        }
+      }
+      case 2: {
+        if (b === 1) {
+          return "A";
+        } else if (b === 3) {
+          return "B";
+        }
+      }
+      case 3: {
+        if (b === 1) {
+          return "B";
+        } else if (b === 2) {
+          return "A";
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    answer += checkVictory(a[i], b[i]);
+  }
 
   return answer;
 }
