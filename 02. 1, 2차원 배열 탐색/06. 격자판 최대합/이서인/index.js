3a545f6068d98fda5d89f7561fe528b465706292
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-13 11:45:28 +0900

    feat: 2-6. 격자판 최대합 풀이

--- 02. 1, 2차원 배열 탐색/06. 격자판 최대합/이서인/index.js
+++ 02. 1, 2차원 배열 탐색/06. 격자판 최대합/이서인/index.js
@@ -1,6 +1,28 @@
 function solution(arr) {
   let answer = Number.MIN_SAFE_INTEGER;
 
+  const maxWidth = Math.max(
+    ...arr.map((el) => el.reduce((acc, cur) => acc + cur), 0)
+  );
+
+  const result = new Array(arr[0].length).fill(0);
+  arr.forEach((el) => {
+    el.forEach((el, index) => {
+      result[index] += el;
+    });
+  });
+  const maxHeight = Math.max(...result);
+
+  const leftDiagonal = arr
+    .map((el, index) => el[index])
+    .reduce((acc, cur) => acc + cur, 0);
+
+  const rightDiagonal = arr
+    .map((el, index) => el[arr[0].length - index - 1])
+    .reduce((acc, cur) => acc + cur, 0);
+
+  answer = Math.max(maxWidth, maxHeight, leftDiagonal, rightDiagonal);
+
   return answer;
 }
 
