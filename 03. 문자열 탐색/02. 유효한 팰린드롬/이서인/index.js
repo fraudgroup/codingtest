26f9f2a23a235c1691b8bcfd241cf546caa70883
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-16 12:38:30 +0900

    feat: 3-2. 유효한 팰린드롬 1차 풀이

--- 03. 문자열 탐색/02. 유효한 팰린드롬/이서인/index.js
+++ 03. 문자열 탐색/02. 유효한 팰린드롬/이서인/index.js
@@ -1,8 +1,37 @@
 function solution(s) {
-  let answer = 'YES';
-
-  return answer;
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const filteredS = [...s.toLowerCase()]
+    .filter((el) => alphabet.includes(el))
+    .join("");
+  return filteredS === [...filteredS].reverse().join("") ? "YES" : "NO";
 }
 
-let str = 'found7, time: study; Yduts; emit, 7Dnuof';
+let str = "found7, time: study; Yduts; emit, 7Dnuof";
 console.log(solution(str));
