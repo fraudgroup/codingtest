f7e7b7fac32f617116a982c4ad08df2f089ed341
김보경 <rlaqhrud6263@naver.com>
2024-07-17 11:50:45 +0900

    Feat: 01-02 풀이

--- 01. 기본문제 풀이/02. 삼각형 판별하기/김보경/index.js
+++ 01. 기본문제 풀이/02. 삼각형 판별하기/김보경/index.js
@@ -1,7 +1,13 @@
 function solution(a, b, c) {
-  let answer = 'YES';
+  let answer = "YES";
 
-  return answer;
+  let arr = [a, b, c];
+  let maxValeu = Math.max(...arr);
+  let result = arr.reduce((acc, el) => {
+    return el !== maxValeu ? acc + el : acc;
+  });
+
+  return result > maxValeu ? answer : "NO";
 }
 
 console.log(solution(13, 33, 17));
