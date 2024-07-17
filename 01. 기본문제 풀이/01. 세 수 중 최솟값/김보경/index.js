53e12ef57aa945a9ca9458d6640c5377dbc567aa
김보경 <rlaqhrud6263@naver.com>
2024-07-17 11:29:52 +0900

    Feat: 01-01 풀이

--- 01. 기본문제 풀이/01. 세 수 중 최솟값/김보경/index.js
+++ 01. 기본문제 풀이/01. 세 수 중 최솟값/김보경/index.js
@@ -1,7 +1,29 @@
 function solution(a, b, c) {
   let answer;
+  if (a < b) {
+    answer = a;
+  } else {
+    answer = b;
+  }
+
+  if (answer < c) {
+    return;
+  } else {
+    answer = c;
+  }
 
   return answer;
 }
 
 console.log(solution(2, 5, 1));
+
+// const input = require("fs")
+//   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+//   .toString()
+//   .trim()
+//   .split(" ")
+//   .map(Number);
+
+// let solution = input.reduce((acc, el) => acc + el, 0);
+
+// console.log(solution);
