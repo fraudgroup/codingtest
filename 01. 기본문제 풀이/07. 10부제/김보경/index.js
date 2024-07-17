b08755ff0c77df7fae7305ec468159947b78b0ec
김보경 <rlaqhrud6263@naver.com>
2024-07-17 12:35:58 +0900

    Feat: 01-07 다른 풀이 추가

--- 01. 기본문제 풀이/07. 10부제/김보경/index.js
+++ 01. 기본문제 풀이/07. 10부제/김보경/index.js
@@ -6,6 +6,13 @@ function solution(day, arr) {
     add[1] == day ? answer++ : answer;
   });
 
+  // 다른 풀이
+  answer = 0;
+
+  arr.forEach((el) => {
+    el % 10 === day ? answer++ : answer;
+  });
+
   return answer;
 }
 
