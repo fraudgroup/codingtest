6382e2e5535106277a768bb068f6042f7f4dfe2c
김보경 <rlaqhrud6263@naver.com>
2024-07-18 11:46:53 +0900

    Feat: 01-17 풀이

--- 01. 기본문제 풀이/17. 중복단어제거/김보경/index.js
+++ 01. 기본문제 풀이/17. 중복단어제거/김보경/index.js
@@ -1,7 +1,10 @@
 function solution(s) {
   let answer;
 
+  let arr = [...new Set(s)];
+  answer = arr;
+
   return answer;
 }
-let str = ['good', 'time', 'good', 'time', 'student'];
+let str = ["good", "time", "good", "time", "student"];
 console.log(solution(str));
