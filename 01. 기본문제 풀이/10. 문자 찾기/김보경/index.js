79a4bf9987d32375d32328da4b38afae9aa382e7
김보경 <rlaqhrud6263@naver.com>
2024-07-18 11:04:55 +0900

    Feat: 01-10 풀이

--- 01. 기본문제 풀이/10. 문자 찾기/김보경/index.js
+++ 01. 기본문제 풀이/10. 문자 찾기/김보경/index.js
@@ -1,8 +1,12 @@
 function solution(s, t) {
   let answer = 0;
 
+  let arr = s.split("").forEach((el) => {
+    el === t && answer++;
+  });
+
   return answer;
 }
 
-let str = 'COMPUTERPROGRAMMING';
-console.log(solution(str, 'R'));
+let str = "COMPUTERPROGRAMMING";
+console.log(solution(str, "R"));
