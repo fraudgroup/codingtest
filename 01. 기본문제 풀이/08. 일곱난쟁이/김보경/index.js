daec764691c5967b338b9125f85317ae48724cb2
김보경 <rlaqhrud6263@naver.com>
2024-07-18 10:44:45 +0900

    Feat: 01-08 풀이

--- 01. 기본문제 풀이/08. 일곱난쟁이/김보경/index.js
+++ 01. 기본문제 풀이/08. 일곱난쟁이/김보경/index.js
@@ -1,6 +1,33 @@
 function solution(arr) {
   let answer = arr;
 
+  let nineHeight = arr.reduce((acc, el) => acc + el, 0);
+
+  let fakers = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let sevenHeight = 100;
+    let eightHeight = nineHeight - arr[i];
+    let faker = [];
+    faker.push(i);
+    if (eightHeight > sevenHeight) {
+      for (let j = 0; j < arr.length; j++) {
+        if (j === i) {
+          continue;
+        }
+
+        if (eightHeight - arr[j] === sevenHeight) {
+          faker.push(j);
+        }
+      }
+    }
+
+    if (faker.length >= 2) {
+      fakers.push(faker);
+    }
+  }
+
+  console.log(fakers);
   return answer;
 }
 
