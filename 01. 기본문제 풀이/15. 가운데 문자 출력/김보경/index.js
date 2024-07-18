2125b28820e420ca5096b436e94badfbe320fe8d
김보경 <rlaqhrud6263@naver.com>
2024-07-18 11:36:15 +0900

    Feat: 01-15 풀이

--- 01. 기본문제 풀이/15. 가운데 문자 출력/김보경/index.js
+++ 01. 기본문제 풀이/15. 가운데 문자 출력/김보경/index.js
@@ -1,6 +1,14 @@
 function solution(s) {
   let answer;
 
+  if (s.length % 2 === 0) {
+    let start = s.length / 2 - 1;
+    answer = s.slice(start, start + 2);
+  } else {
+    let start = Math.floor(s.length / 2);
+    answer = s.slice(start, start + 1);
+  }
+
   return answer;
 }
-console.log(solution('study'));
+console.log(solution("study"));
