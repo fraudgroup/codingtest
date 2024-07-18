e65405e8bb0fb2f07eaa8cd1305c4e8d510a4f6d
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-18 11:50:51 +0900

    feat: 3-4. 가장 짧은 문자거리 풀이

--- 03. 문자열 탐색/04. 가장 짧은 문자거리/이서인/index.js
+++ 03. 문자열 탐색/04. 가장 짧은 문자거리/이서인/index.js
@@ -1,8 +1,25 @@
 function solution(s, t) {
-  let answer = [];
+  const L = s.length;
+  const distances = new Array(L).fill(Infinity);
 
-  return answer;
+  let lastPosition = -Infinity;
+  for (let i = 0; i < L; i++) {
+    if (s[i] === t) {
+      lastPosition = i;
+    }
+    distances[i] = i - lastPosition;
+  }
+
+  lastPosition = Infinity;
+  for (let i = L - 1; i >= 0; i--) {
+    if (s[i] === t) {
+      lastPosition = i;
+    }
+    distances[i] = Math.min(distances[i], lastPosition - i);
+  }
+
+  return distances;
 }
 
-let str = 'teachermode';
-console.log(solution(str, 'e'));
+let str = "teachermode";
+console.log(solution(str, "e").join(" "));
