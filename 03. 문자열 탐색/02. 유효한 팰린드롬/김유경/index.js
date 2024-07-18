23b181ccec828aab2f1b5ac1001e4a1a3bbcb8bd
codingaring <sksms2104@naver.com>
2024-07-18 15:55:28 +0900

    yukyoung_3-2

--- 03. 문자열 탐색/02. 유효한 팰린드롬/김유경/index.js
+++ 03. 문자열 탐색/02. 유효한 팰린드롬/김유경/index.js
@@ -1,8 +1,54 @@
 function solution(s) {
-  let answer = 'YES';
+  const ALPHABET = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let L = s.length;
+  let strArray = new Array({ length: L });
+  let reverseArray = new Array({ length: L });
 
-  return answer;
+  for (let i = 0; i < L; i++) {
+    const lowerAlphabet = s[i].toLowerCase();
+    if (ALPHABET.includes(lowerAlphabet)) {
+      strArray[i] = lowerAlphabet;
+      reverseArray[L - 1 - i] = lowerAlphabet;
+    }
+  }
+
+  return strArray.join("") === reverseArray.join("") ? "YES" : "No";
 }
 
-let str = 'found7, time: study; Yduts; emit, 7Dnuof';
-console.log(solution(str));
+/** 입출력 테스트 */
+let str1 = "found7, time: study; Yduts; emit, 7Dnuof"; //Yes
+let str2 = "A man, a plan, a canal, Panama"; //Yes
+let str3 = "Python is fun!"; //No
+let str4 = "No 77lemon, no 9melon"; //Yes
+
+console.log(solution(str1));
+console.log(solution(str2));
+console.log(solution(str3));
+console.log(solution(str4));
