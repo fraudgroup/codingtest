fbd855e857994d4b975ef5b3f8c5931cf10b6e47
devwqc <devwqc@gmail.com>
2024-07-19 02:35:44 +0900

    [정봉찬] 3-4. 가장 짧은 문자거리 2회차

--- 03. 문자열 탐색/04. 가장 짧은 문자거리/정봉찬/index.js
+++ 03. 문자열 탐색/04. 가장 짧은 문자거리/정봉찬/index.js
@@ -39,3 +39,49 @@ function solution(s, t) {
 
 let str = 'teachermode';
 console.log(solution(str, 'e'));
+
+/*
+✅문제 풀이 날짜: 2024-07-19
+
+💡문제 분석 요약
+  - 문자열의 각 문자가 문자 t와 떨어진 최소거리를 출력한다.
+  - 문자열과 문자는 소문자로만 구성된다.
+  - 문자열의 길이는 100을 넘지 않는다.
+
+💡알고리즘 설계
+  - 문자열을 반복문 돌면서 왼쪽에서부터 t와의 거리를 계산한다.
+  - 문자열을 반복문 돌면서 오른쪽에서부터 t와의 거리를 계산하며 왼쪽에서부터 돌았을 때와 비교해서 최소 값을 부여한다.
+*/
+
+function solution2(s, t) {
+  let answer = [];
+  const sLength = s.length;
+  let distance = sLength + 1;
+
+  for (let i = 0; i < sLength; i++) {
+    if (s[i] === t) {
+      distance = 0;
+    } else {
+      distance++;
+    }
+
+    answer.push(distance);
+  }
+
+  distance = sLength + 1;
+
+  for (let i = sLength - 1; i >= 0; i--) {
+    if (s[i] === t) {
+      distance = 0;
+    } else {
+      distance++;
+      answer[i] = Math.min(answer[i], distance);
+    }
+  }
+
+  answer = answer.join('');
+
+  return answer;
+}
+
+console.log(solution2(str, 'e'));
