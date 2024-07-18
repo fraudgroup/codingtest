9b18ff39072e8ec918dc51c0b6a723d0e7d44efd
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-18 16:49:54 +0900

    feat: 가장 짧은 문자거리 풀이

--- 03. 문자열 탐색/04. 가장 짧은 문자거리/윤아영/index.js
+++ 03. 문자열 탐색/04. 가장 짧은 문자거리/윤아영/index.js
@@ -1,7 +1,29 @@
 function solution(s, t) {
-  let answer = [];
+  let answer = new Array(s.length).fill(0);
+  let tPrevIndex = -1;
+  let tNextIndex = s.indexOf(t);
 
-  return answer;
+  for (let i = 0; i < s.length; i++) {
+    if (i === tNextIndex) {
+      tPrevIndex = i;
+      tNextIndex = s.indexOf(t, i + 1);
+      continue;
+    }
+
+    if (tPrevIndex === -1) {
+      answer[i] = Math.abs(tNextIndex - i)
+      continue;
+    }
+    
+    if (tNextIndex === -1) {      
+      answer[i] = Math.abs(tPrevIndex - i)
+      continue;
+    }
+    
+    answer[i] = Math.min(Math.abs(tPrevIndex - i), Math.abs(tNextIndex - i));
+  }
+
+  return answer.join(" ");
 }
 
 let str = 'teachermode';
