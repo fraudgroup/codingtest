88dc26abc1c234cb016765ed7521479022219c1e
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-19 23:44:45 +0900

    refact: 변수명 수정

--- 03. 문자열 탐색/05. 문자열 압축/윤아영/index.js
+++ 03. 문자열 탐색/05. 문자열 압축/윤아영/index.js
@@ -1,17 +1,17 @@
 function solution(s) {
   let answer = "";
-  let currentString = "";
+  let repeatString = "";
   let repeatNumber = 1;
 
   for (let i = 0; i < s.length; i++) {
-    if (currentString === s[i]) {
+    if (repeatString === s[i]) {
       repeatNumber++;
       continue;
     }
 
     answer += repeatNumber === 1 ? "" : repeatNumber;
     answer += s[i];
-    currentString = s[i];
+    repeatString = s[i];
     repeatNumber = 1;
   }
 
