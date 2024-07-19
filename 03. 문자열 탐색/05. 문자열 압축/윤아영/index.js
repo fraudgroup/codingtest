b9a7d1563888b8cc2d5453e3b6436ae886e6942c
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-19 23:39:37 +0900

    feat: 문자열 압축 풀이

--- 03. 문자열 탐색/05. 문자열 압축/윤아영/index.js
+++ 03. 문자열 탐색/05. 문자열 압축/윤아영/index.js
@@ -1,8 +1,22 @@
 function solution(s) {
-  let answer = '';
+  let answer = "";
+  let currentString = "";
+  let repeatNumber = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (currentString === s[i]) {
+      repeatNumber++;
+      continue;
+    }
+
+    answer += repeatNumber === 1 ? "" : repeatNumber;
+    answer += s[i];
+    currentString = s[i];
+    repeatNumber = 1;
+  }
 
   return answer;
 }
 
-let str = 'KKHSSSSSSSE';
+let str = "KKHSSSSSSSE";
 console.log(solution(str));
