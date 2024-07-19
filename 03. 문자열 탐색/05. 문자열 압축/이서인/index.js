7eb80ae2b5573017ec7ac0f0f5eef47435746c13
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-19 18:08:06 +0900

    fix: 3-5. 문자열 압축 풀이 수정

--- 03. 문자열 탐색/05. 문자열 압축/이서인/index.js
+++ 03. 문자열 탐색/05. 문자열 압축/이서인/index.js
@@ -1,12 +1,17 @@
 function solution(s) {
-  const objectS = {};
-  [...s].map((el) => {
-    objectS[el] = (objectS[el] || 0) + 1;
-  });
-  return Object.entries(objectS)
-    .map(([key, value]) => (value === 1 ? key : key + value))
+  const result = [...s].reduce((acc, cur, i, arr) => {
+    if (i === 0 || cur !== arr[i - 1]) {
+      acc.push({ cur, count: 1 });
+    } else {
+      acc[acc.length - 1].count++;
+    }
+    return acc;
+  }, []);
+
+  return result
+    .map(({ cur, count }) => (count > 1 ? cur + count : cur))
     .join("");
 }
 
-let str = "KKHSSSSSSSE";
+let str = "KKHSSSSSSSEKKK";
 console.log(solution(str));
