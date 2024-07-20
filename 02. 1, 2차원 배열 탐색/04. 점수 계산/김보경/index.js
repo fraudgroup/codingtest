7ea35546e0313b207b5c3155cdf45eaf021a0173
김보경 <rlaqhrud6263@naver.com>
2024-07-20 12:17:42 +0900

    Feat: 02-04 풀이

--- 02. 1, 2차원 배열 탐색/04. 점수 계산/김보경/index.js
+++ 02. 1, 2차원 배열 탐색/04. 점수 계산/김보경/index.js
@@ -1,5 +1,15 @@
 function solution(arr) {
   let answer = 0;
+  let stack = 0;
+
+  arr.forEach((el) => {
+    if (el > 0) {
+      stack++;
+      answer += stack;
+    } else {
+      stack = 0;
+    }
+  });
 
   return answer;
 }
