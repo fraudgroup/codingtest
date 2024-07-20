b8e27b1ef38c1aceea270cdbc9f6782d320bb367
김보경 <rlaqhrud6263@naver.com>
2024-07-20 20:16:28 +0900

    Feat: 02-07 풀이

--- 02. 1, 2차원 배열 탐색/07. 봉우리/김보경/index.js
+++ 02. 1, 2차원 배열 탐색/07. 봉우리/김보경/index.js
@@ -1,5 +1,33 @@
 function solution(arr) {
   let answer = 0;
+  let n = arr.length;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < dx.length; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+
+        if (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < n &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          flag = 0;
+          break;
+        }
+      }
+
+      if (flag) {
+        answer++;
+      }
+    }
+  }
 
   return answer;
 }
