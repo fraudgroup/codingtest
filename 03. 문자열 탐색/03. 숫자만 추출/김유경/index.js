9f2a9f9017e78efdb80260391a5b0190776fe4b0
codingaring <sksms2104@naver.com>
2024-07-21 00:03:29 +0900

    yukyoung_3-3

--- 03. 문자열 탐색/03. 숫자만 추출/김유경/index.js
+++ 03. 문자열 탐색/03. 숫자만 추출/김유경/index.js
@@ -1,8 +1,15 @@
 function solution(str) {
-  let answer = '';
+  let answer = Number(str.replace(/[^0-9]/g, ""));
+
+  while (answer > 100000000) {
+    answer = Number(answer.toString().slice(0, -1));
+  }
 
   return answer;
 }
 
-let str = 'g0en2T0s8eSoft';
-console.log(solution(str));
+let str = "g0en2T0s8eSoft";
+let soBig = "abc1234567890def";
+
+console.log(solution(str)); // 208
+console.log(solution(soBig)); // 12345678
