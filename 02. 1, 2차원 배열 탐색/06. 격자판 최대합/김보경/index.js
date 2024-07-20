88854bfc7956a3a16a52eae74183bc0d7c4b89f5
김보경 <rlaqhrud6263@naver.com>
2024-07-20 13:53:54 +0900

    Feat: 02-06 풀이

--- 02. 1, 2차원 배열 탐색/06. 격자판 최대합/김보경/index.js
+++ 02. 1, 2차원 배열 탐색/06. 격자판 최대합/김보경/index.js
@@ -1,7 +1,31 @@
 function solution(arr) {
-  let answer = Number.MIN_SAFE_INTEGER;
+  // 각 행의 합
+  let rowSum = [];
+  arr.forEach((el) => {
+    let sum = el.reduce((acc, element) => acc + element, 0);
+    rowSum.push(sum);
+  });
 
-  return answer;
+  // 각 열의 합
+  let columnSum = [];
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+
+    for (let j = 0; j < arr[i].length; j++) {
+      sum += arr[j][i];
+    }
+
+    columnSum.push(sum);
+  }
+
+  // 두 대각선의 합
+  let crossSum = [0, 0];
+  arr.forEach((el, i) => {
+    crossSum[0] += el[i];
+    crossSum[1] += el.reverse()[i];
+  });
+
+  return Math.max(...rowSum, ...columnSum, ...crossSum);
 }
 
 let arr = [
