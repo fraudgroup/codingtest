afa9746a9238363d6d13f46ad1915ff1d035489f
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-22 08:41:32 +0900

    feat: 4-2. 뒤집은 소수 풀이

--- 04. 완전탐색(블루투포스)/02. 뒤집은 소수/이서인/index.js
+++ 04. 완전탐색(블루투포스)/02. 뒤집은 소수/이서인/index.js
@@ -1,7 +1,20 @@
 function solution(arr) {
-  let answer = [];
-
-  return answer;
+  const reversedNumbers = arr.map((el) =>
+    parseInt([...(el + "")].reverse().join(""))
+  );
+  return reversedNumbers
+    .map((number) => {
+      if (number > 1) {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+          if (number % i === 0) {
+            return 0;
+          }
+        }
+        return number;
+      }
+    })
+    .filter((n) => n !== 0)
+    .join(" ");
 }
 
 let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
