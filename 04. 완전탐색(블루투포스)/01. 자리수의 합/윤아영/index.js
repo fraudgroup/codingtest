594e34607ad6aafba5ae45e76d783bc86f81d0e3
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-22 15:23:50 +0900

    feat: 04-01. 자릿수의 합 풀이

--- 04. 완전탐색(블루투포스)/01. 자리수의 합/윤아영/index.js
+++ 04. 완전탐색(블루투포스)/01. 자리수의 합/윤아영/index.js
@@ -1,7 +1,34 @@
 function solution(n, arr) {
-  let answer;
+  let maxNumberSum = {
+    sum: Number.MIN_SAFE_INTEGER,
+    number: null,
+  };
 
-  return answer;
+  for (let i = 0; i < n; i++) {
+    let sum = 0;
+    let number = arr[i];
+
+    while (number > 0) {
+      sum += number % 10;
+      number = Math.floor(number / 10);
+    }
+
+    if (sum < maxNumberSum.sum) {
+      continue;
+    }
+
+    if (maxNumberSum.sum === sum) {
+      maxNumberSum.number = Math.max(maxNumberSum.number, arr[i]);
+      continue;
+    }
+
+    maxNumberSum = {
+      sum,
+      number: arr[i],
+    };
+  }
+
+  return maxNumberSum.number;
 }
 
 let arr = [128, 460, 603, 40, 521, 137, 123];
