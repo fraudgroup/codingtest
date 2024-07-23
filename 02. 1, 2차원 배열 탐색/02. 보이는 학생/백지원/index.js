d1ace5979a7003855c27c1b8585cfa26a922fc62
seolsis <seoljw1005@naver.com>
2024-07-24 00:26:17 +0900

    2-2. 보이는 학생

--- 02. 1, 2차원 배열 탐색/02. 보이는 학생/백지원/index.js
+++ 02. 1, 2차원 배열 탐색/02. 보이는 학생/백지원/index.js
@@ -1,6 +1,13 @@
 function solution(arr) {
   let answer = 1;
+  let max = arr[0];
 
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      answer++;
+      max = arr[i];
+    }
+  }
   return answer;
 }
 
