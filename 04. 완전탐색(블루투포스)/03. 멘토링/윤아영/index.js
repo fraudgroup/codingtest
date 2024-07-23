3f203fc26001ef7e018022dd29aec075f6ca65cb
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-24 01:39:24 +0900

    feat: 멘토링 풀이

--- 04. 완전탐색(블루투포스)/03. 멘토링/윤아영/index.js
+++ 04. 완전탐색(블루투포스)/03. 멘토링/윤아영/index.js
@@ -1,7 +1,22 @@
 function solution(test) {
-  let answer = 0;
+  const numberOfStudents = test[0].length;
+  const mentoNmentee = {};
 
-  return answer;
+  for (let i = 0; i < test.length; i++) {
+    for (j = 0; j < numberOfStudents; j++) {
+      if (i === 0) {
+        mentoNmentee[test[i][j]] = test[i].slice(j + 1);
+        continue;
+      }
+
+      const below = new Set(test[i].slice(j + 1));
+      mentoNmentee[test[i][j]] = mentoNmentee[test[i][j]].filter((v) =>
+        below.has(v)
+      );
+    }
+  }
+
+  return Object.values(mentoNmentee).flat().length;
 }
 
 let arr = [
