f795b22f3e25eec73e8dc0a1880f59ad6273f6e8
codingaring <sksms2104@naver.com>
2024-07-23 21:28:46 +0900

    yukyoung_3-4 : 문제 풀이

--- 03. 문자열 탐색/04. 가장 짧은 문자거리/김유경/index.js
+++ 03. 문자열 탐색/04. 가장 짧은 문자거리/김유경/index.js
@@ -1,8 +1,29 @@
 function solution(s, t) {
   let answer = [];
+  let tIndexList = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) {
+      tIndexList.push(i);
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let distance;
+    for (let j = 0; j < tIndexList.length; j++) {
+      if (s[i] === t) {
+        answer[i] = 0;
+      }
+      distance =
+        Math.abs(i - tIndexList[j]) > distance
+          ? distance
+          : Math.abs(i - tIndexList[j]);
+      answer[i] = distance;
+    }
+  }
 
   return answer;
 }
 
-let str = 'teachermode';
-console.log(solution(str, 'e'));
+let str = "teachermode";
+console.log(solution(str, "e"));
