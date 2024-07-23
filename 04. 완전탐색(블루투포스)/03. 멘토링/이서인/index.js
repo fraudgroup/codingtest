e5e2dc7d52148917dbf6d4b3564e5db2694572df
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-23 23:22:14 +0900

    feat: 4-3. 멘토링 풀이

--- 04. 완전탐색(블루투포스)/03. 멘토링/이서인/index.js
+++ 04. 완전탐색(블루투포스)/03. 멘토링/이서인/index.js
@@ -1,6 +1,31 @@
 function solution(test) {
+  const S = test[0].length;
+  const T = test.length;
   let answer = 0;
 
+  let rankings = Array.from({ length: T }, () => Array(S));
+
+  test.forEach((testResult, testIndex) => {
+    testResult.forEach((student, rankIndex) => {
+      rankings[testIndex][student - 1] = rankIndex + 1;
+    });
+  });
+  for (let mentor = 0; mentor < S; mentor++) {
+    for (let mentee = 0; mentee < S; mentee++) {
+      if (mentor !== mentee) {
+        let isValidPair = true;
+        for (let testIndex = 0; testIndex < T; testIndex++) {
+          if (rankings[testIndex][mentor] >= rankings[testIndex][mentee]) {
+            isValidPair = false;
+            break;
+          }
+        }
+        if (isValidPair) {
+          answer++;
+        }
+      }
+    }
+  }
   return answer;
 }
 
