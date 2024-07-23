0591191660b543a3389269c85c169626fd531229
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-23 11:02:50 +0900

    feat: 뒤집은 소수 풀이

--- 04. 완전탐색(블루투포스)/02. 뒤집은 소수/윤아영/index.js
+++ 04. 완전탐색(블루투포스)/02. 뒤집은 소수/윤아영/index.js
@@ -1,7 +1,17 @@
 function solution(arr) {
-  let answer = [];
+  const reverseNumbers = arr.map((v) => +[...String(v)].reverse().join(""));
 
-  return answer;
+  return reverseNumbers.filter((v) => {
+    if (v < 2) return false;
+
+    for (let i = 2; i < Math.sqrt(v); i++) {
+      if (!(v % i)) {
+        return false;
+      }
+    }
+
+    return true;
+  });
 }
 
 let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
