09aa7a9bdba246fe676780bc2d9661c2842ec604
김보경 <rlaqhrud6263@naver.com>
2024-07-24 09:16:12 +0900

    Feat: 03-01 풀이

--- 03. 문자열 탐색/01. 회문문자열/김보경/index.js
+++ 03. 문자열 탐색/01. 회문문자열/김보경/index.js
@@ -1,8 +1,17 @@
 function solution(s) {
-  let answer = 'YES';
+  let answer = "YES";
+
+  let arr = s.toLowerCase();
+  let n = Math.floor(Math.sqrt(arr.length, 2));
+
+  for (let i = 0; i < n; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) {
+      answer = "NO";
+    }
+  }
 
   return answer;
 }
 
-let str = 'goooG';
+let str = "goooG";
 console.log(solution(str));
