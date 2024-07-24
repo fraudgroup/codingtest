9a77082062a4f20a04b60b062b93503e0a7de5e9
minyoung <alsdyd1021@naver.com>
2024-07-24 23:06:56 +0900

    [민영] 2-2. 보이는 학생

--- 02. 1, 2차원 배열 탐색/02. 보이는 학생/김민영/index.js
+++ 02. 1, 2차원 배열 탐색/02. 보이는 학생/김민영/index.js
@@ -1,5 +1,13 @@
 function solution(arr) {
   let answer = 1;
+  let currentMaxHeight = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > currentMaxHeight) {
+      answer += 1;
+      currentMaxHeight = arr[i];
+    }
+  }
 
   return answer;
 }
