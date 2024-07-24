01c7f1c087a56877fe64ab19d153d94830d50d36
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-24 11:22:59 +0900

    feat: 4-4. 졸업선물 풀이

--- 04. 완전탐색(블루투포스)/04. 졸업선물/이서인/index.js
+++ 04. 완전탐색(블루투포스)/04. 졸업선물/이서인/index.js
@@ -1,7 +1,33 @@
 function solution(m, product) {
-  let answer = 0;
+  let sums = [];
 
-  return answer;
+  for (let i = 0; i < product.length; i++) {
+    sums.push(product[i][0] + product[i][1]);
+  }
+
+  sums = sums.sort((a, b) => a - b);
+
+  let buyProducts = 0;
+  let currentPrice = 0;
+
+  for (let i = 0; i < sums.length; i++) {
+    if (currentPrice + sums[i] <= m) {
+      currentPrice += sums[i];
+      buyProducts++;
+    } else {
+      break;
+    }
+  }
+
+  if (buyProducts < product.length) {
+    const nextProductPrice = product[buyProducts][0];
+    const nextDeliveryPrice = product[buyProducts][1];
+
+    if (currentPrice + nextProductPrice / 2 + nextDeliveryPrice <= m) {
+      buyProducts++;
+    }
+  }
+  return buyProducts;
 }
 
 let arr = [
@@ -12,3 +38,12 @@ let arr = [
   [10, 3],
 ];
 console.log(solution(28, arr));
+// console.log(
+//   solution(41, [
+//     [8, 6],
+//     [2, 2],
+//     [12, 1],
+//     [4, 5],
+//     [4, 3],
+//   ])
+// );
