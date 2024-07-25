6a6d240e2b46063879bd7b2abe2b0b06affb3b1d
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-25 11:23:22 +0900

    K번째 큰 수 풀이

--- 04. 완전탐색(블루투포스)/05. K번째 큰 수/윤아영/index.js
+++ 04. 완전탐색(블루투포스)/05. K번째 큰 수/윤아영/index.js
@@ -1,7 +1,18 @@
 function solution(n, k, card) {
-  let answer;
+  card.sort((a, b) => b - a);
+  let order = 0;
 
-  return answer;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let l = j + 1; l < n; l++) {
+        if (++order === k) {
+          return card[i] + card[j] + card[l];
+        }
+      }
+    }
+  }
+
+  return -1;
 }
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
