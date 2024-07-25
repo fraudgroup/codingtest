fd07d185a2581358d9fee56af31b56eef3ec6d3e
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-25 09:05:07 +0900

    feat: 4-5. K번째 큰 수 풀이

--- 04. 완전탐색(블루투포스)/05. K번째 큰 수/이서인/index.js
+++ 04. 완전탐색(블루투포스)/05. K번째 큰 수/이서인/index.js
@@ -1,7 +1,16 @@
 function solution(n, k, card) {
-  let answer;
+  let sums = [];
+  card.forEach((firstCard, i) => {
+    card.forEach((secondCard, j) => {
+      if (i === j) return;
+      card.forEach((thirdCard, k) => {
+        if (i === k || j === k) return;
+        sums.push(firstCard + secondCard + thirdCard);
+      });
+    });
+  });
 
-  return answer;
+  return [...new Set(sums)].sort((a, b) => b - a)[k - 1];
 }
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
