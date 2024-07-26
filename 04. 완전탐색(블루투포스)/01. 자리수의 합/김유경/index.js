6eb805f5b6db05190017aca0c0d0aa5a4cff7ccc
codingaring <sksms2104@naver.com>
2024-07-26 15:28:42 +0900

    yukyoung_4-1

--- 04. 완전탐색(블루투포스)/01. 자리수의 합/김유경/index.js
+++ 04. 완전탐색(블루투포스)/01. 자리수의 합/김유경/index.js
@@ -1,7 +1,20 @@
 function solution(n, arr) {
-  let answer;
+  let maxSum = 0;
+  let maxNum = 0;
 
-  return answer;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    (arr[i] + "").split("").forEach((value) => {
+      sum += +value;
+    });
+
+    if (sum > maxSum || (sum == maxSum && arr[i] > maxNum)) {
+      maxSum = sum;
+      maxNum = arr[i];
+    }
+  }
+
+  return maxNum;
 }
 
 let arr = [128, 460, 603, 40, 521, 137, 123];
