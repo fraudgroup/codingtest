5764e720f281437a0a4b5dfefe9498825097896a
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-26 21:36:34 +0900

    feat: 5-1. 두 배열 합치기 투포인터 알고리즘 적용 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/01. 두 배열 합치기/이서인/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/01. 두 배열 합치기/이서인/index.js
@@ -1,5 +1,19 @@
 function solution(arr1, arr2) {
-  return [...arr1, ...arr2].sort((a, b) => a - b);
+  const answer = [];
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  while (pointer1 < arr1.length || pointer2 < arr2.length) {
+    if (
+      pointer2 === arr2.length ||
+      (pointer1 < arr1.length && arr1[pointer1] <= arr2[pointer2])
+    ) {
+      answer.push(arr1[pointer1++]);
+    } else {
+      answer.push(arr2[pointer2++]);
+    }
+  }
+  return answer;
 }
 
 let a = [1, 3, 5];
