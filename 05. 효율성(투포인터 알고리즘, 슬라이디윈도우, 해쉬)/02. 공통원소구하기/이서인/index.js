1ba67cf310d8918239ce28ac1cc4025debde8036
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-07-27 10:19:08 +0900

    feat: 5-2. 공통원소 구하기

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/이서인/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/이서인/index.js
@@ -1,5 +1,22 @@
 function solution(arr1, arr2) {
   let answer = [];
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  while (pointer1 < arr1.length && pointer2 < arr2.length) {
+    if (arr1[pointer1] === arr2[pointer2]) {
+      answer.push(arr1[pointer1]);
+      pointer1++;
+      pointer2++;
+    } else if (arr1[pointer1] < arr2[pointer2]) {
+      pointer1++;
+    } else {
+      pointer2++;
+    }
+  }
 
   return answer;
 }
