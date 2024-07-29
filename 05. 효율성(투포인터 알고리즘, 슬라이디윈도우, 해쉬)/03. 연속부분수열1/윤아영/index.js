aa91b1d3d3f7350d2aaa29144a50f5409cb2226e
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-29 11:38:57 +0900

    feat: 연속부분수열1 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/03. 연속부분수열1/윤아영/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/03. 연속부분수열1/윤아영/index.js
@@ -1,5 +1,24 @@
 function solution(m, arr) {
   let answer = 0;
+  let sum = 0;
+  let point1 = 0;
+  let point2 = 0;
+
+  while (point1 < arr.length) {
+    sum += arr[point2];
+
+    if (sum < m && point2 < arr.length - 1) {
+      point2++;
+      continue;
+    }
+
+    if (sum === m) {
+      answer++;
+    }
+
+    sum = 0;
+    point2 = ++point1;
+  }
 
   return answer;
 }
