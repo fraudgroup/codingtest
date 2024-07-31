9e29a0bb610530a0923aa268776a06e79b9fcd82
ayoung-iya <rhaehfdl1879@naver.com>
2024-07-31 10:22:50 +0900

    feat: 최대 매출 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/05. 최대 매출/윤아영/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/05. 최대 매출/윤아영/index.js
@@ -1,7 +1,16 @@
 function solution(k, arr) {
-  let answer;
+  let pointer = k;
+  let sum = arr.slice(0, k).reduce((acc, cur) => acc + cur, 0);
+  let maxSales = sum;
 
-  return answer;
+  while (pointer < arr.length) {
+    sum -= arr[pointer - k];
+    sum += arr[pointer++];
+
+    maxSales = Math.max(sum, maxSales);
+  }
+
+  return maxSales;
 }
 
 let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
