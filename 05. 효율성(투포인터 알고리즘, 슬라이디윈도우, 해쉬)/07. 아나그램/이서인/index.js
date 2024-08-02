0418391d2c8b1fbdd28131c2d8017dc4b9e710fe
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-02 09:52:37 +0900

    feat: 5-7. 아나그램 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/07. 아나그램/이서인/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/07. 아나그램/이서인/index.js
@@ -1,9 +1,30 @@
 function solution(str1, str2) {
-  let answer = 'YES';
+  let answer = "YES";
+
+  const hash1 = new Map();
+  const hash2 = new Map();
+
+  [...str1].forEach((el) => {
+    hash1.set(el, (hash1.get(el) || 0) + 1);
+  });
+
+  [...str2].forEach((el) => {
+    hash2.set(el, (hash2.get(el) || 0) + 1);
+  });
+
+  if (hash1.size !== hash2.size) {
+    return "NO";
+  }
+
+  for (const [key, value] of hash1) {
+    if (hash2.get(key) !== value) {
+      return "NO";
+    }
+  }
 
   return answer;
 }
 
-let a = 'AbaAeCe';
-let b = 'baeeACA';
+let a = "AbaAeCe";
+let b = "baeeACA";
 console.log(solution(a, b));
