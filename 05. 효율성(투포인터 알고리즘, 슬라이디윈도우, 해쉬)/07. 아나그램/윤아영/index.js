e5a306f26c7be1bf79abc328ad0c2cccb0a26ac5
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-02 12:01:09 +0900

    오답 풀이: map의 요소는 두 개의 요소를 가진 배열의 형태다

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/07. 아나그램/윤아영/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/07. 아나그램/윤아영/index.js
@@ -8,11 +8,11 @@ function solution(str1, str2) {
 
   for (let i = 0; i < str1.length; i++) {
     anagram1.set(str1[i], (anagram1.get(str1[i]) || 0) + 1);
-    anagram2.set(str1[i], (anagram2.get(str2[i]) || 0) + 1);
+    anagram2.set(str2[i], (anagram2.get(str2[i]) || 0) + 1);
   }
 
-  for (const alphabet of anagram1) {
-    if (anagram1.get(alphabet) !== anagram2.get(alphabet)) {
+  for (const [alphabet, number] of anagram1) {
+    if (anagram2.get(alphabet) !== number) {
       return "NO";
     }
   }
