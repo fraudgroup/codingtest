6d8b81cda262f20bf20094ba4b8b8b4ebccc50eb
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-03 11:53:20 +0900

    feat: 5-8. 모든 아나그램 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/08. 모든 아나그램/이서인/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/08. 모든 아나그램/이서인/index.js
@@ -1,9 +1,46 @@
 function solution(s, t) {
-  let answer = 0;
+  // 슬라이딩윈도우로 length 같은 부분 배열 구하고 해시 만들기
+  let hashes = [];
+  let answer = [];
 
-  return answer;
+  // t.length와 같은 길이의 부분 배열 구하기
+  [...s].forEach((_, index) => {
+    if (index + t.length <= [...s].length) {
+      let window = [...s].slice(index, index + t.length);
+      let hash = new Map();
+
+      window.forEach((el) => {
+        hash.set(el, (hash.get(el) || 0) + 1);
+      });
+
+      hashes.push(hash);
+    }
+  });
+
+  // hash1: 구해야 할 t의 해시
+  const hash1 = new Map();
+  [...t].forEach((el) => {
+    hash1.set(el, (hash1.get(el) || 0) + 1);
+  });
+
+  // hashes를 순회하면서 hash의 키와 값이 hash와 같은지 확인
+  hashes.forEach((hash) => {
+    let isMatch = true;
+
+    for (const [key, value] of hash) {
+      if (hash1.get(key) !== value) {
+        isMatch = false;
+        break;
+      }
+    }
+
+    if (isMatch) {
+      answer.push(hash);
+    }
+  });
+  return answer.length;
 }
 
-let a = 'bacaAacba';
-let b = 'abc';
+let a = "bacaAacba";
+let b = "abc";
 console.log(solution(a, b));
