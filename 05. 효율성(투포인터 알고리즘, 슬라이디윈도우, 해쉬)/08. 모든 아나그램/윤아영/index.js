c8f054493e3f6ebf45e44a7775c7da63602b4651
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-04 23:23:30 +0900

    feat: 모든 아나그램 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/08. 모든 아나그램/윤아영/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/08. 모든 아나그램/윤아영/index.js
@@ -1,9 +1,48 @@
 function solution(s, t) {
   let answer = 0;
+  const tAlphabetMap = new Map();
+  const sAlphabetMap = new Map();
+
+  const isAnagram = (sMap, tMap) => {
+    if (sMap.size !== tMap.size) {
+      return false;
+    }
+
+    for (let [key, value] of tMap) {
+      if (sMap.get(key) !== value) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  [...t].forEach((alphabet) => tAlphabetMap.set(alphabet, (tAlphabetMap.get(alphabet) || 0) + 1));
+  [...s.slice(0, t.length)].forEach((alphabet) => sAlphabetMap.set(alphabet, (sAlphabetMap.get(alphabet) || 0) + 1));
+
+  for (let i = t.length; i <= s.length; i++) {
+    if (isAnagram(sAlphabetMap, tAlphabetMap)) {
+      answer++;
+    }
+
+    if (!s[i]) {
+      break;
+    }
+
+    const pointer = i - t.length;
+
+    sAlphabetMap.set(s[i], (sAlphabetMap.get(s[i]) || 0) + 1);
+
+    if (sAlphabetMap.get(s[pointer]) === 1) {
+      sAlphabetMap.delete(s[pointer]);
+    } else {
+      sAlphabetMap.set(s[pointer], sAlphabetMap.get(s[pointer]) - 1);
+    }
+  }
 
   return answer;
 }
 
-let a = 'bacaAacba';
-let b = 'abc';
+let a = "bacaAacba";
+let b = "abc";
 console.log(solution(a, b));
