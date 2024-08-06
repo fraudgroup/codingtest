bca8127a26232758902853504d8b2fe7a565c1e5
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-06 10:27:34 +0900

    feat: 괄호문자제거 풀이

--- 06. 자료구조(스택, 큐)/02. 괄호문자제거/윤아영/index.js
+++ 06. 자료구조(스택, 큐)/02. 괄호문자제거/윤아영/index.js
@@ -1,8 +1,25 @@
 function solution(s) {
-  let answer;
+  const bracket = [];
+  let answer = "";
+
+  for (const character of s) {
+    if (character === "(") {
+      bracket.push(character);
+      continue;
+    }
+
+    if (character === ")") {
+      bracket.pop();
+      continue;
+    }
+
+    if (!bracket.length) {
+      answer += character;
+    }
+  }
 
   return answer;
 }
 
-let str = '(A(BC)D)EF(G(H)(IJ)K)LM(N)';
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
 console.log(solution(str));
