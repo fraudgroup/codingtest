5e3d52eb9feaf3e2a6bbd36108181346ac4b3277
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-08 10:21:01 +0900

    feat: 6-4. 후위식 연산

--- 06. 자료구조(스택, 큐)/04. 후위식 연산/이서인/index.js
+++ 06. 자료구조(스택, 큐)/04. 후위식 연산/이서인/index.js
@@ -1,8 +1,48 @@
 function solution(s) {
-  let answer;
+  let stack = [];
+  const REGEX = /^[0-9]$/;
 
-  return answer;
+  [...s].forEach((el) => {
+    if (REGEX.test(el)) {
+      stack.push(el * 1);
+    } else {
+      let a = stack.pop();
+      let b = stack.pop();
+
+      switch (el) {
+        case "+":
+          stack.push(b + a);
+          break;
+        case "-":
+          stack.push(b - a);
+          break;
+        case "*":
+          stack.push(b * a);
+          break;
+        case "/":
+          stack.push(b / a);
+          break;
+      }
+    }
+  });
+  return stack[0];
 }
 
-let str = '352+*9-';
+let str = "352+*9-";
 console.log(solution(str));
+
+// let answer;
+// let stack = [];
+// let values = [...s];
+// const REGEX = /^[0-9]$/g;
+
+// values.forEach((value) => {
+//   if (!REGEX.test(value)) {
+//     while (stack.length > 0 && REGEX.test(value[stack.length - 1]))
+//       stack.pop();
+//   } else {
+//     stack.push(value);
+//   }
+// });
+// console.log(stack);
+// return answer;
