63875377d2dc8c22112c3055c33fe240c2acea22
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-08 16:44:39 +0900

    feat: 후위식 연산 풀이

--- 06. 자료구조(스택, 큐)/04. 후위식 연산/윤아영/index.js
+++ 06. 자료구조(스택, 큐)/04. 후위식 연산/윤아영/index.js
@@ -1,8 +1,32 @@
 function solution(s) {
-  let answer;
+  const stack = [];
 
-  return answer;
+  const calculation = (operation, num1, num2) => {
+    switch (operation) {
+      case "+":
+        return +num1 + +num2;
+      case "-":
+        return num1 - num2;
+      case "*":
+        return num1 * num2;
+      case "/":
+        return num1 / num2;
+    }
+  };
+
+  for (const char of s) {
+    if (Number.isInteger(+char)) {
+      stack.push(+char);
+      continue;
+    }
+
+    const [num1, num2] = stack.splice(stack.length - 2, 2);
+
+    stack.push(calculation(char, num1, num2));
+  }
+
+  return stack[0];
 }
 
-let str = '352+*9-';
+let str = "352+*9-";
 console.log(solution(str));
