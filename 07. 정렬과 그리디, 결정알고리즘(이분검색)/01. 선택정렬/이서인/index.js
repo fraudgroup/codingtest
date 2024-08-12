7cabd9f92c1adf8ece29f94c3b15cc8f229191cb
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-12 11:14:40 +0900

    feat: 7-1. 선택정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/01. 선택정렬/이서인/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/01. 선택정렬/이서인/index.js
@@ -1,7 +1,20 @@
 function solution(arr) {
-  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
 
-  return answer;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      let swap = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = swap;
+    }
+  }
+  return arr;
 }
 
 let arr = [13, 5, 11, 7, 23, 15];
