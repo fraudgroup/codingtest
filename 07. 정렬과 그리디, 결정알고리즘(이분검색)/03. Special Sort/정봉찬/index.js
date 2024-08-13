8ee0216f77c9924a9031e027a657ccae14044f11
devwqc <devwqc@gmail.com>
2024-08-13 11:04:37 +0900

    [정봉찬] 7-3. Special Sort 선택정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/03. Special Sort/정봉찬/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/03. Special Sort/정봉찬/index.js
@@ -1,5 +1,46 @@
+/*
+✅문제 제목: Special Sort
+
+✅문제 유형: 정렬
+
+✅문제 풀이 날짜: 2024-08-13
+
+💡문제 분석 요약
+  - 음수 정수는 앞쪽에 양의 정수는 뒷쪽에 있어야 한다.
+  - 양의 정수와 음의 정수의 순서에는 변함이 없어야 한다.
+
+💡알고리즘 설계
+  - ⭐️ 선택정렬
+  - 배열의 길이 - 1 만큼을 반복하는 반복문을 선언한다.
+  - 최소값의 인덱스를 저장할 변수 minIndex를 선언하고 외부 반복문의 인덱스로 초기화한다.
+  - 외부 반복문의 인덱스 + 1을 시작으로 배열의 길이 - 1 만큼을 반복하는 중첩 반복문을 선언한다.
+  - minIndex의 값보다 중첩 반복문의 현재 값이 더 작다면 minIndex를 중첩 반복문의 인덱스로 교체한다.
+  - 이때 minIndex의 값과 중첩 반복문의 현재 값이 모두 음수라면 절대값 비교를 해야 한다.
+  - 중첩 반복문 종료 후 외부 반복문의 인덱스 값과 minIndex 값을 스왑한다.
+*/
+
 function solution(arr) {
   let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < 0 && arr[minIndex] < 0) {
+        if (Math.abs(arr[j]) >= Math.abs(arr[minIndex])) {
+          continue;
+        }
+      }
+
+      if (arr[j] >= arr[minIndex]) {
+        continue;
+      }
+
+      minIndex = j;
+    }
+
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
 
   return answer;
 }
