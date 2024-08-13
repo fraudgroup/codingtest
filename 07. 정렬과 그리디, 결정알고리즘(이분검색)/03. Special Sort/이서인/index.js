610fdfadbeb846e555cae79e3e3d542c0aec5066
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-13 09:37:58 +0900

    refactor: 7-3. 정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/03. Special Sort/이서인/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/03. Special Sort/이서인/index.js
@@ -1,18 +1,17 @@
 function solution(arr) {
-  // let answer = arr;
-  // for (let i = 0; i < arr.length - 1; i++) {
-  //   for (let j = 0; j < arr.length - 1 - i; j++) {
-  //     if (arr[j] > 0) {
-  //       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-  //     }
-  //   }
-  // }
-  // return answer;
-
-  const minusArr = [...arr].filter((el) => el <= 0);
-  const plusArr = [...arr].filter((el) => el > 0);
-  return [...minusArr, ...plusArr];
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+  // const minusArr = [...arr].filter((el) => el <= 0);
+  // const plusArr = [...arr].filter((el) => el > 0);
+  // return [...minusArr, ...plusArr];
 }
 
-let arr = [1, 2, 3, -3, -2, 5, 6, -6, 0];
+let arr = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(arr));
