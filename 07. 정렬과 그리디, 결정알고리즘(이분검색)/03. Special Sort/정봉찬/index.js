6e5204d8e5590292a4597e12036a54489c5a0bbe
devwqc <devwqc@gmail.com>
2024-08-13 11:14:51 +0900

    [정봉찬] 7-3. Special Sort 버블정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/03. Special Sort/정봉찬/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/03. Special Sort/정봉찬/index.js
@@ -47,3 +47,45 @@ function solution(arr) {
 
 let arr = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(arr));
+
+/*
+✅문제 제목: Special Sort
+
+✅문제 유형: 정렬
+
+✅문제 풀이 날짜: 2024-08-13
+
+💡문제 분석 요약
+  - 음수 정수는 앞쪽에 양의 정수는 뒷쪽에 있어야 한다.
+  - 양의 정수와 음의 정수의 순서에는 변함이 없어야 한다.
+
+💡알고리즘 설계
+  - ⭐️ 버블정렬
+  - i = 0부터 배열의 길이 - 1 만큼을 반복하는 반복문을 선언한다.
+  - j = 0부터 배열의 길이 - i - 1 만큼을 반복하는 반복문을 선언한다.
+  - j의 값이 j + 1보다 크다면 j와 j + 1의 값을 교체한다. 단, j의 값과 j + 1의 값이 음수이면 절대값으로 비교한다.
+*/
+
+function solution2(arr) {
+  let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < 0 && arr[j + 1] < 0) {
+        if (arr[j] * -1 < arr[j + 1] * -1) {
+          continue;
+        }
+      }
+
+      if (arr[j] < arr[j + 1]) {
+        continue;
+      }
+
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution2(arr));
