1f228d114623f381742eb779e10b0d77738730fe
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-14 10:34:12 +0900

    feat: 삽입정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/04. 삽입정렬/윤아영/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/04. 삽입정렬/윤아영/index.js
@@ -1,7 +1,20 @@
 function solution(arr) {
-  let answer = arr;
+  for (let i = 1; i < arr.length; i++) {
+    let pointer = i;
 
-  return answer;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[pointer] >= arr[j]) {
+        break;
+      }
+
+      if (arr[pointer] < arr[j]) {
+        [arr[j], arr[pointer]] = [arr[pointer], arr[j]];
+        pointer -= 1;
+      }
+    }
+  }
+
+  return arr;
 }
 
 let arr = [11, 7, 5, 6, 10, 9];
