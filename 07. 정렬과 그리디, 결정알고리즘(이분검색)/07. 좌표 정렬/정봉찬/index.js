d3fbfde29e24fa9c018a33ee564c1a6a2c976cca
devwqc <devwqc@gmail.com>
2024-08-15 15:16:27 +0900

    [정봉찬] 7-7. 좌표 정렬 버블정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬/정봉찬/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬/정봉찬/index.js
@@ -45,3 +45,45 @@ let arr = [
   [3, 6],
 ];
 console.log(solution(arr)); // [[1, 2], [1, 3], [2, 5], [2, 7], [3, 6]]
+
+/*
+✅문제 제목: 좌표 정렬
+
+✅문제 유형: 정렬
+
+✅문제 풀이 날짜: 2024-08-15
+
+💡문제 분석 요약
+  - 좌표 리스트가 주어졌을 때 좌표를 오름차순으로 정렬하세요.
+  - 정렬기준은 x 값에 의해서 정렬하고, x 값이 같은 경우 y 값에 의해 정렬한다.
+
+💡알고리즘 설계
+  - ⭐️버블정렬
+  - i = 0부터 배열의 길이 - 1 만큼 반복문을 돈다.
+  - j = 0부터 배열의 길이 - i - 1 만큼 반복문을 돈다.
+  - j[0]의 값이 j + 1[0]보다 작다면 continue 한다.
+  - j[0]과 j + 1[0]의 값이 같을 때, j[1]의 값이 j + 1[1]보다 작다면 continue 한다.
+  - 그 외에는 j와 j + 1의 값을 교체한다.
+*/
+
+function solution2(arr) {
+  let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j][0] < arr[j + 1][0]) {
+        continue;
+      }
+
+      if (arr[j][1] < [j + 1][1]) {
+        continue;
+      }
+
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution2(arr)); // [[1, 2], [1, 3], [2, 5], [2, 7], [3, 6]]
