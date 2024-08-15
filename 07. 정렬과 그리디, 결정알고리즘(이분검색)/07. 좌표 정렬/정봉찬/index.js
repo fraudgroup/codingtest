ad1e8ac21caf9bcbf58bcb121bb180c0abfff572
devwqc <devwqc@gmail.com>
2024-08-15 15:29:27 +0900

    [정봉찬] 7-7. 좌표 정렬 선택정렬 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬/정봉찬/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬/정봉찬/index.js
@@ -87,3 +87,51 @@ function solution2(arr) {
 }
 
 console.log(solution2(arr)); // [[1, 2], [1, 3], [2, 5], [2, 7], [3, 6]]
+
+/*
+✅문제 제목: 좌표 정렬
+
+✅문제 유형: 정렬
+
+✅문제 풀이 날짜: 2024-08-15
+
+💡문제 분석 요약
+  - 좌표 리스트가 주어졌을 때 좌표를 오름차순으로 정렬하세요.
+  - 정렬기준은 x 값에 의해서 정렬하고, x 값이 같은 경우 y 값에 의해 정렬한다.
+
+💡알고리즘 설계
+  - ⭐️선택정렬
+  - i = 0부터 배열의 길이 - 1만큼 반복문을 돈다.
+  - 최소값의 인덱스를 담을 minIndex를 선언하고 i로 초기화한다.
+  - j = i + 1부터 배열의 길이만큼 중첩 반복문을 돈다.
+  - j[0]의 값이 minIndex[0]보다 크다면 continue한다.
+  - j[1]의 값이 minIndex[1]보다 크다면 continue한다.
+  - 그 외에는 j의 값을 minIndex에 넣어준다.
+  - 중첩 반복문이 종료되면 i와 minIndex의 값을 교체한다.
+*/
+
+function solution3(arr) {
+  let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j][0] > arr[minIndex][0]) {
+        continue;
+      }
+
+      if (arr[j][1] > arr[minIndex][1]) {
+        continue;
+      }
+
+      minIndex = j;
+    }
+
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+
+  return answer;
+}
+
+console.log(solution3(arr)); // [[1, 2], [1, 3], [2, 5], [2, 7], [3, 6]]
