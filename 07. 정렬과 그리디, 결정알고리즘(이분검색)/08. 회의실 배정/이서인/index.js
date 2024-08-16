85bc3e06d6041972c18e8ea7d51620c0dfeefaff
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-16 11:01:32 +0900

    feat: 7-8. 회의실 배정 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/08. 회의실 배정/이서인/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/08. 회의실 배정/이서인/index.js
@@ -1,5 +1,18 @@
 function solution(meeting) {
   let answer = 0;
+  let preEnd = 0;
+
+  const sortedArr = meeting.sort((a, b) =>
+    a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]
+  );
+
+  sortedArr.forEach((time) => {
+    const [start, end] = time;
+    if (start >= preEnd) {
+      preEnd = end;
+      answer++;
+    }
+  });
 
   return answer;
 }
