047cad6cbe1123b5f157c172a83f028d3de2f639
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-16 10:08:39 +0900

    refactor: pr 리뷰 반영-조건문 수정

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/06. 장난꾸러기 현수/윤아영/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/06. 장난꾸러기 현수/윤아영/index.js
@@ -2,7 +2,7 @@ function solution(arr) {
   let answer = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] <= arr[i + 1]) {
+    if (arr[i] < arr[i + 1]) {
       continue;
     }
 
@@ -23,4 +23,6 @@ function solution(arr) {
 }
 
 let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+let arr2 = [120, 130, 150, 150, 130, 150];
 console.log(solution(arr));
+console.log(solution(arr2));
