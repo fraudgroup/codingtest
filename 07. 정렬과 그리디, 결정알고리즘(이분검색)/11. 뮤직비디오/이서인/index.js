5f77bc3c27978932f1402aff28b264bb7bbd6d88
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-18 01:04:45 +0900

    feat: 7-11 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/11. 뮤직비디오/이서인/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/11. 뮤직비디오/이서인/index.js
@@ -1,5 +1,36 @@
 function solution(m, songs) {
-  let answer;
+  let answer = 0;
+  let start = Math.max(...songs);
+  let end = arr.reduce((a, b) => a + b, 0);
+  let DVD = [];
+
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    let sum = 0;
+    let count = 1;
+
+    songs.forEach((song) => {
+      if (sum + song > mid) {
+        DVD.push(sum);
+        count += 1;
+        sum = song;
+      } else {
+        sum += song;
+      }
+    });
+
+    DVD.push(sum);
+
+    if (m === count) {
+      answer = Math.max(...DVD);
+      break;
+    } else if (m > count) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+    DVD = [];
+  }
 
   return answer;
 }
