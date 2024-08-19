6e4b9f85f765ffbc4abf68b3b3ad98a9a59f019a
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-20 01:13:21 +0900

    feat: 7-12 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/12. 마구간 정하기/이서인/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/12. 마구간 정하기/이서인/index.js
@@ -1,6 +1,30 @@
-function solution(c, stable) {
-  let answer;
+function count(positions, distance) {
+  let cnt = 1;
+  let lastPosition = positions[0];
+  for (let i = 1; i < positions.length; i++){
+    if (positions[i] - lastPosition >= distance) {
+      cnt++;
+      lastPosition = positions[i]
+    }
+  }
+  return cnt;
+}
 
+function solution(c, stable) {
+  stable.sort((a, b) => a - b)
+  let answer = 0;
+  let start = 1;
+  let end = stable[stable.length - 1]
+  
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
   return answer;
 }
 
