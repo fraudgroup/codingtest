620d4418883a201d0743989c2bbe6642f825adac
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-19 17:26:33 +0900

    feat: 결혼식 풀이

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/09. 결혼식/윤아영/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/09. 결혼식/윤아영/index.js
@@ -1,5 +1,20 @@
 function solution(times) {
+  times.sort((a, b) => a[1] - b[1] || a[0] - b[1]);
+
   let answer = Number.MIN_SAFE_INTEGER;
+  let people = 0;
+  let endTime = times[0][1];
+
+  for (const [start, end] of times) {
+    if (endTime > start) {
+      people++;
+      answer = Math.max(answer, people);
+      continue;
+    }
+
+    people--;
+    endTime = end;
+  }
 
   return answer;
 }
