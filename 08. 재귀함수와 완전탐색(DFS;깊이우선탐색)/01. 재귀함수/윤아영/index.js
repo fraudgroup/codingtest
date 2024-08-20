6797c5ae8e3637269541bc54f3504e12defb1fec
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-20 21:46:06 +0900

    feat: 재귀함수 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/01. 재귀함수/윤아영/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/01. 재귀함수/윤아영/index.js
@@ -1,6 +1,13 @@
 function solution(n) {
-  function DFS(L) {}
-  DFS(n);
+  function DFS(L) {
+    if (L === 1) {
+      return L;
+    }
+
+    return `${DFS(L - 1)} ${L}`;
+  }
+
+  console.log(DFS(n));
 }
 
 solution(3);
