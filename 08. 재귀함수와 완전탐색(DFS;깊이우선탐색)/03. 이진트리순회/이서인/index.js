17d9a0b9a77a05149eed25390e891a923927146f
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-21 01:57:54 +0900

    refactor: 8-3 후위순회 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/03. 이진트리순회/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/03. 이진트리순회/이서인/index.js
@@ -3,9 +3,9 @@ function solution(n) {
   function DFS(v) {
     if (v > 7) return;
     else {
-      answer += (v + ' ');
       DFS(v * 2);
       DFS(v * 2 + 1);
+      answer += (v + ' ');
     }
   }
   DFS(n);
