a1c3222221c6f4e0fcb5856e9a07a6290ae25c63
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-21 01:50:20 +0900

    feat: 8-3 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/03. 이진트리순회/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/03. 이진트리순회/이서인/index.js
@@ -1,6 +1,14 @@
 function solution(n) {
-  let answer = '';
-
+  let answer = "";
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      answer += (v + ' ');
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+    }
+  }
+  DFS(n);
   return answer;
 }
 
