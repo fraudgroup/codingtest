6524e34c948ef069a3c8c04517e809a8603485a9
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-22 15:07:50 +0900

    feat: 바둑이 승차 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/06. 바둑이 승차/윤아영/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/06. 바둑이 승차/윤아영/index.js
@@ -1,6 +1,19 @@
 function solution(c, arr) {
   let answer = Number.MIN_SAFE_INTEGER;
 
+  const DFS= (index, path) => {
+    const sum = path.reduce((acc, cur) => acc + cur, 0);
+    if (sum <= c) {
+      answer = Math.max(answer, sum);
+    }
+
+    for(let i = index; i < arr.length; i++) {
+      DFS(i + 1, path.concat(arr[i]))
+    }
+  }
+
+  DFS(0, []);
+
   return answer;
 }
 
