b1c6f01afd25b0da8146ae3392c42e1235f4fbce
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-23 03:34:03 +0900

    feat: 8-5 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/05. 합이 같은 부분집합/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/05. 합이 같은 부분집합/이서인/index.js
@@ -1,5 +1,20 @@
 function solution(arr) {
   let answer = 'NO';
+  const totalSum = arr.reduce((a, b) => a + b, 0);
+  
+  function DFS(index, currentSum) {
+    if (currentSum > totalSum / 2) return;
+    if (currentSum === totalSum / 2) {
+      answer = 'YES';
+      return;
+    }
+    if (index >= arr.length) return;
+
+    DFS(index + 1, currentSum + arr[index]);
+    DFS(index + 1, currentSum)
+  }
+
+  DFS(0, 0)
 
   return answer;
 }
