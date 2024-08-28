806cb049fd9bbd7343f90aa65846bf1565bdb2a9
김보경 <rlaqhrud6263@naver.com>
2024-08-28 17:50:10 +0900

    Feat: 05-06 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/06. 학급 회장(해쉬)/김보경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/06. 학급 회장(해쉬)/김보경/index.js
@@ -1,8 +1,25 @@
 function solution(s) {
   let answer;
+  let results = {};
+  let maxValue = 0;
+
+  [...s].forEach((el) => {
+    if (el in results) {
+      results[el]++;
+    } else {
+      results[el] = 1;
+    }
+  });
+
+  for (let key in results) {
+    if (results[key] > maxValue) {
+      maxValue = results[key];
+      answer = key;
+    }
+  }
 
   return answer;
 }
 
-let str = 'BACBACCACCBDEDE';
+let str = "BACBACCACCBDEDE";
 console.log(solution(str));
