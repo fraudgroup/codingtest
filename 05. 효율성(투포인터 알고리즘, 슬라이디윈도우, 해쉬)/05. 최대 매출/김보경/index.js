119e468ac4749a7e783f2f92ae79c31b9985baad
김보경 <rlaqhrud6263@naver.com>
2024-08-28 17:14:53 +0900

    Feat: 05-05 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/05. 최대 매출/김보경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/05. 최대 매출/김보경/index.js
@@ -1,5 +1,18 @@
 function solution(k, arr) {
-  let answer;
+  let answer = 0;
+  let stack = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    stack.push(arr[i]);
+
+    if (stack.length > k) {
+      stack.shift();
+    }
+    if (stack.length === k) {
+      let sum = stack.reduce((acc, el) => acc + el, 0);
+      answer = sum > answer ? sum : answer;
+    }
+  }
 
   return answer;
 }
