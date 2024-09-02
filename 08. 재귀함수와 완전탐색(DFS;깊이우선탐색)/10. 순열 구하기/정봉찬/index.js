a9da0505ed9ce4492d10cbaa27850202f01c390f
devwqc <devwqc@gmail.com>
2024-09-02 21:44:39 +0900

    chore: 테스트 소스 제거

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/10. 순열 구하기/정봉찬/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/10. 순열 구하기/정봉찬/index.js
@@ -1,28 +1,7 @@
 function solution(m, arr) {
   let answer = [];
 
-  const DFS = (array, restArray) => {
-    if (array.length === m) {
-      answer.push(array);
-      return;
-    }
-
-    for (let i = 0; i < restArray.length; i++) {
-      DFS(
-        [...array, restArray[i]],
-        [...restArray.slice(0, i), ...restArray.slice(i + 1)]
-      );
-    }
-  };
-
-  DFS([], arr);
-
-  for (let i = 0; i < answer.length; i++) {
-    console.log(answer[i]);
-  }
-
-  return answer.length;
+  return answer;
 }
 
 let arr = [3, 6, 9];
-console.log(solution(2, arr));
