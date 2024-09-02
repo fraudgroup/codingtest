4f7776421274516e367eb6d6310fb7decbb68034
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-02 20:17:41 +0900

    feat: 순열 구하기 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/10. 순열 구하기/윤아영/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/10. 순열 구하기/윤아영/index.js
@@ -1,6 +1,19 @@
 function solution(m, arr) {
   let answer = [];
 
+  const DFS = (index, array) => {
+    if (array.length === m) {
+      answer.push(array);
+      return;
+    }
+
+    for(let i = index; i < arr.length; i++) {
+      DFS(i + 1, [...array, arr[i]]);
+    }
+  }
+
+  DFS(0, []);
+
   return answer;
 }
 
