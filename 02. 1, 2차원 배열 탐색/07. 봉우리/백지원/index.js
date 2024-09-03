c1e99dad4ccfac6ccc133510a4caa3090ba6d825
seolsis <seoljw1005@naver.com>
2024-09-03 17:09:35 +0900

    2-7. 봉우리

--- 02. 1, 2차원 배열 탐색/07. 봉우리/백지원/index.js
+++ 02. 1, 2차원 배열 탐색/07. 봉우리/백지원/index.js
@@ -1,6 +1,36 @@
 function solution(arr) {
   let answer = 0;
+  let n = arr.length;
 
+  let dx = [-1, 1, 0, 0];
+  let dy = [0, 0, -1, 1];
+
+  let outlineArr = new Array(n + 2).fill(0).map(() => new Array(n + 2).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      outlineArr[i + 1][j + 1] = arr[i][j];
+    }
+  }
+
+  // 봉우리 탐색
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      let isPeak = true; // 초기값을 봉우리로
+
+      // 상하좌우 탐색
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+
+        if (outlineArr[nx][ny] >= outlineArr[i][j]) {
+          isPeak = false;
+          break;
+        }
+      }
+      if (isPeak) answer++;
+    }
+  }
   return answer;
 }
 
