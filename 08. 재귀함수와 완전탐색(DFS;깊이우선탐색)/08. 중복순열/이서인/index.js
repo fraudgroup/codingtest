c096775ea26656c82ae132b2657e78e833a52173
Seoin <seoin2269@naver.com>
2024-09-04 11:19:04 +0900

    refactor: 변수명 수정

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/08. 중복순열/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/08. 중복순열/이서인/index.js
@@ -1,12 +1,11 @@
 function solution(n, m) {
   let answer = [];
 
-  function DFS(depth, part = "") {
-    if (depth === m) return part && answer.push(part)
+  function DFS(numbers, part = "") {
+    if (numbers === m) return part && answer.push(part)
     Array.from({ length: n }, (_, i) => i + 1).forEach((el) => {
-      DFS(depth + 1, part + el)
+      DFS(numbers + 1, part + el)
     })
-
   }
   DFS(0, "");
 
