94ba4f4b57c6eacca821608ecc8ec9752888d2bf
Seoin <seoin2269@naver.com>
2024-09-04 10:34:44 +0900

    feat: 8-8 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/08. 중복순열/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/08. 중복순열/이서인/index.js
@@ -1,7 +1,16 @@
 function solution(n, m) {
   let answer = [];
 
-  return answer;
+  function DFS(depth, part = "") {
+    if (depth === m) return part && answer.push(part)
+    Array.from({ length: n }, (_, i) => i + 1).forEach((el) => {
+      DFS(depth + 1, part + el)
+    })
+
+  }
+  DFS(0, "");
+
+  return answer.join("\n") + "\n" + answer.length;
 }
 
 console.log(solution(3, 2));
