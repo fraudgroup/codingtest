395fb66f41404e58b0670e1310b7669ce8b7bd7d
minyoung <alsdyd1021@naver.com>
2024-09-05 15:37:13 +0900

    [민영] 2-4. 점수 계산

--- 02. 1, 2차원 배열 탐색/04. 점수 계산/김민영/index.js
+++ 02. 1, 2차원 배열 탐색/04. 점수 계산/김민영/index.js
@@ -1,5 +1,15 @@
 function solution(arr) {
   let answer = 0;
+  let count = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      answer += count + 1;
+      count++;
+    } else {
+      count = 0;
+    }
+  }
 
   return answer;
 }
