f239ef9a33f7e8b209aac6e50db1e72d50082e9f
Seoin <seoin2269@naver.com>
2024-09-05 12:23:28 +0900

    feat: 8-9 첫 번째 풀이(스택오버플로우 발생)

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/09. 동전교환/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/09. 동전교환/이서인/index.js
@@ -1,6 +1,19 @@
 function solution(m, arr) {
   let answer = Number.MAX_SAFE_INTEGER;
 
+  function DFS(number, coins = []) {
+    let price = coins.reduce((a, b) => a + b, 0);
+    if (price > m) return;
+    if (price === m) {
+      answer = Math.min(answer, coins.length)
+      return;
+    }
+    if (number > arr.length) return;
+
+    DFS(number, [...coins, arr[number]])    
+    DFS(number + 1, coins)    
+  }
+  DFS(0, [])
   return answer;
 }
 
