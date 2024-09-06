5dce695e7069781b993cf31fd2d1ce54292f293a
Seoin <seoin2269@naver.com>
2024-09-06 10:44:11 +0900

    feat: 8-10 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/10. 순열 구하기/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/10. 순열 구하기/이서인/index.js
@@ -1,7 +1,15 @@
 function solution(m, arr) {
   let answer = [];
 
-  return answer;
+  function DFS(index, part = "") {
+    if (part.length > 1 && part[0] === part[1]) return;
+    if (index === m) return part && answer.push(part);
+    arr.forEach(el => {
+      DFS(index + 1, part + el)
+    })
+  }
+  DFS(0, "")
+  return answer.join("\n") + "\n" + answer.length;
 }
 
 let arr = [3, 6, 9];
