e496ab6782f1b68f7d05f902b0823436ba019b30
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-06 11:01:21 +0900

    feat: 조합 구하기 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/14. 조합 구하기/윤아영/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/14. 조합 구하기/윤아영/index.js
@@ -1,6 +1,19 @@
 function solution(n, m) {
   let answer = [];
 
+  const DFS = (index, array) => {
+    if (array.length === m) {
+      answer.push(array);
+      return;
+    }
+
+    for (let i = index; i <= n; i++) {
+      DFS(i + 1, [...array, i]);
+    }
+  };
+
+  DFS(1, []);
+
   return answer;
 }
 
