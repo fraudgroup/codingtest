a426469b06913b26379e401d813f9b96f6c97578
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-06 11:29:48 +0900

    feat: 수들의 조합 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/15. 수들의 조합/윤아영/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/15. 수들의 조합/윤아영/index.js
@@ -1,6 +1,19 @@
 function solution(n, k, arr, m) {
   let answer = 0;
 
+  const DFS = (index, array) => {
+    if (array.length === k) {
+      answer = array.reduce((acc, cur) => acc + cur, 0) % m === 0 ? answer + 1 : answer;
+      return;
+    }
+
+    for (let i = index; i < arr.length; i++) {
+      DFS(i + 1, [...array, arr[i]]);
+    }
+  };
+
+  DFS(0, []);
+
   return answer;
 }
 
