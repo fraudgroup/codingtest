c776684a098f688eb335bb3efbf7fa9a838854a6
devwqc <devwqc@gmail.com>
2024-09-07 16:01:02 +0900

    [정봉찬] 9-2. 경로탐색(DFS) 다른 풀이

--- 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/02. 경로탐색(DFS)/정봉찬/index.js
+++ 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/02. 경로탐색(DFS)/정봉찬/index.js
@@ -65,3 +65,62 @@ let arr = [
   [4, 5],
 ];
 console.log(solution(5, arr)); // 6
+
+/*
+✅문제 제목: 경로탐색(DFS)
+
+✅문제 유형: DFS
+
+✅문제 풀이 날짜: 2024-09-07
+
+💡문제 분석 요약
+  - 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
+  - 정점의 수 N(1<=N<=20), 간선의 수 M
+
+💡알고리즘 설계
+  - 노드는 v, 간선은 e로 표현한다.
+  - 각 노드가 방문할 수 있는 노드를 저장할 이차원 배열 graph를 선언하고 0으로 초기화한다.
+  - 노드의 방문 여부를 저장할 ch 배열을 선언하고 0으로 초기화한다.
+  - arr을 반복문 돌면서 graph에 방문 여부를 1로 할당한다.
+  - 재귀함수의 매개변수로 현재 노드를 나타내는 v를 받는다.
+  - 첫 재귀함수 호출할 때 인수로 1을 넘겨주며 ch[1]을 체크한다.
+  - v가 n에 도달하면 answer를 1 더하고 함수를 종료한다.
+  - 1부터 n만큼 반복문을 돌면서 노드를 탐색한다.
+  - graph를 통해서 현재 노드가 방문할 수 없는 노드면 continue한다.
+  - ch를 통해서 이미 방문한 노드의 경우 continue한다.
+*/
+
+function solution2(n, arr) {
+  let answer = 0;
+
+  const graph = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+  const ch = Array.from({ length: n + 1 }, () => 0);
+
+  for (const [s, e] of arr) {
+    graph[s][e] = 1;
+  }
+
+  function DFS(v) {
+    if (v === n) {
+      answer++;
+      return;
+    }
+
+    for (let i = 1; i <= n; i++) {
+      if (!graph[v][i] || ch[i]) {
+        continue;
+      }
+
+      ch[i] = 1;
+      DFS(i);
+      ch[i] = 0;
+    }
+  }
+
+  ch[1] = 1;
+  DFS(1);
+
+  return answer;
+}
+
+console.log(solution2(5, arr)); // 6
