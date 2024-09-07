38a6d14b22bcdbd031da240c9e83ad77bf0dca29
minyoung <alsdyd1021@naver.com>
2024-09-07 17:32:16 +0900

    [민영] 2-5. 등수구하기

--- 02. 1, 2차원 배열 탐색/05. 등수구하기/김민영/index.js
+++ 02. 1, 2차원 배열 탐색/05. 등수구하기/김민영/index.js
@@ -1,8 +1,15 @@
 function solution(arr) {
+  let sortArr = [...arr].sort((a, b) => b - a);
   let answer = [];
+
+  for (i = 0; i < arr.length; i++) {
+    answer.push(sortArr.indexOf(arr[i]) + 1);
+  }
 
   return answer;
 }
 
 let arr = [87, 89, 92, 100, 76];
+let arr1 = [83, 92, 78, 92, 92]
 console.log(solution(arr));
+console.log(solution(arr1));
