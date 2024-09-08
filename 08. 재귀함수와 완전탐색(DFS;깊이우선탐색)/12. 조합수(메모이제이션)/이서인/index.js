1884fc87e17a91bd2196466eaa54d5250ac09fe1
Seoin <seoin2269@naver.com>
2024-09-08 12:11:22 +0900

    feat: 8-12 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/12. 조합수(메모이제이션)/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/12. 조합수(메모이제이션)/이서인/index.js
@@ -1,7 +1,12 @@
 function solution(n, r) {
   let answer = [];
-
+  function DFS(n, r) {
+    if (r === 0 || n === r) return 1;
+    return DFS(n - 1, r -1) + DFS(n - 1, r)
+  }
+  answer = DFS(n, r)
   return answer;
 }
 
-console.log(solution(5, 3));
+//console.log(solution(5, 3));
+console.log(solution(33, 19));
