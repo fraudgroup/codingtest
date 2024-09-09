7bf70e002fd537cef5fe2e1cc4c801a2bcd51859
Seoin <seoin2269@naver.com>
2024-09-09 17:27:34 +0900

    feat: 8-15 풀이

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/15. 수들의 조합/이서인/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/15. 수들의 조합/이서인/index.js
@@ -1,7 +1,20 @@
 function solution(n, k, arr, m) {
-  let answer = 0;
+  let parts = [];
 
-  return answer;
+  function DFS(index, part = [], count = 0) {
+    if (count === k) {
+      parts.push(part.slice());
+      return;
+    }
+    if (index >= n) return;
+
+    DFS(index + 1, part.concat(arr[index]), count + 1);
+    DFS(index + 1, part, count);
+  }
+
+  DFS(0,[])
+
+  return parts.filter(el => el.reduce((a, b) => a + b, 0) % m === 0).length
 }
 
 let arr = [2, 4, 5, 8, 12];
