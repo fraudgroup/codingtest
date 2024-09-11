a1db95383f892ef928265834f856b41a64808770
김보경 <rlaqhrud6263@naver.com>
2024-09-11 10:46:50 +0900

    Feat: 05-02 다른 풀이 추가

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/김보경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/김보경/index.js
@@ -8,6 +8,27 @@ function solution(arr1, arr2) {
   return answer.sort((a, b) => a - b);
 }
 
+// 다른 풀이 추가
+const solution2 = (arr1, arr2) => {
+  let answer = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+
+  return answer;
+};
+
 let a = [1, 3, 9, 5, 2];
 let b = [3, 2, 5, 7, 8];
 console.log(solution(a, b));
+console.log(solution2(a, b));
