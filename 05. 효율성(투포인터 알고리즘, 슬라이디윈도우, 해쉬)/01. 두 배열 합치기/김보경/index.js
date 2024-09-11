9e5c7a666c533ca8d98665d0b4c9af3f8aabbe3e
김보경 <rlaqhrud6263@naver.com>
2024-09-11 10:22:41 +0900

    Feat: 05-01 다른 풀이 추가

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/01. 두 배열 합치기/김보경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/01. 두 배열 합치기/김보경/index.js
@@ -4,6 +4,26 @@ function solution(arr1, arr2) {
   return answer;
 }
 
+// 다른 풀이 추가
+function solution2(arr1, arr2) {
+  let answer = [];
+
+  let n = arr1.length;
+  let m = arr2.length;
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  while (pointer1 < n && pointer2 < m) {
+    if (arr1[pointer1] <= arr2[pointer2]) answer.push(arr1[pointer1++]);
+    else answer.push(arr2[pointer2++]);
+  }
+  while (pointer1 < n) answer.push(arr1[pointer1++]);
+  while (pointer2 < m) answer.push(arr2[pointer2++]);
+
+  return answer;
+}
+
 let a = [1, 3, 5];
 let b = [2, 3, 6, 7, 9];
 console.log(solution(a, b));
+console.log(solution2(a, b));
