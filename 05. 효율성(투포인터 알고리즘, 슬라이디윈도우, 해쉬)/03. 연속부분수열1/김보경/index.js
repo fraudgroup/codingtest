b8f55be812601f893fd81b9e63e3a0aca1c7531e
김보경 <rlaqhrud6263@naver.com>
2024-09-11 11:24:54 +0900

    Feat: 05-03 다른 풀이 추가

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/03. 연속부분수열1/김보경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/03. 연속부분수열1/김보경/index.js
@@ -19,5 +19,28 @@ function solution(m, arr) {
   return answer;
 }
 
+// 다른 풀이 추가 - 투포인터
+const solution2 = (m, arr) => {
+  let answer = 0;
+
+  let lt = 0;
+  let rt = 0;
+  let sum = 0;
+
+  while (rt < arr.length) {
+    if (sum < m) {
+      sum += arr[rt++];
+    } else if (sum === m) {
+      answer++;
+      sum -= arr[lt++];
+    } else {
+      sum -= arr[lt++];
+    }
+  }
+
+  return answer;
+};
+
 let a = [1, 2, 1, 3, 1, 1, 1, 2];
 console.log(solution(6, a));
+console.log(solution2(6, a));
