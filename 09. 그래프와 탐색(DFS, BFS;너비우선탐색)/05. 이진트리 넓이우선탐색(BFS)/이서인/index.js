f3d00a6fd286a6c79902a97a124324bd158d10cc
Seoin <seoin2269@naver.com>
2024-09-12 11:55:10 +0900

    feat: 9-5 풀이

--- 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/05. 이진트리 넓이우선탐색(BFS)/이서인/index.js
+++ 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/05. 이진트리 넓이우선탐색(BFS)/이서인/index.js
@@ -1,7 +1,17 @@
 function solution() {
-  let answer = '';
+    const queue = [];
+    let answer = ''
+    queue.push(1)
 
-  return answer;
-}
-
+  while (queue.length) {
+    console.log(queue);
+      let v = queue.shift()
+    answer += v + "";
+    for (let newv of [v * 2, v * 2 + 1]) {
+      if (newv > 7) continue;
+      queue.push(newv)
+    }
+  }
+  return answer
+  }
 console.log(solution());
