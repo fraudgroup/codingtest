1bfa33fd3a4fe07715af6d4c0a10a5a10404787d
김보경 <rlaqhrud6263@naver.com>
2024-09-12 16:33:48 +0900

    Feat: 05-08 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/08. 모든 아나그램/김보경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/08. 모든 아나그램/김보경/index.js
@@ -1,9 +1,32 @@
 function solution(s, t) {
   let answer = 0;
+  let sH = new Map();
+  let tH = new Map();
 
-  return answer;
+  [...t].forEach((el) => {
+    sH.set(el, (tH.get(el) || 0) - 1);
+  });
+  console.log(sH);
+
+  let len = t.length - 1;
+  for (let rt = 0; rt < len; rt++) {
+    sH.set(s[rt], (sH.get(s[rt]) || 0) + 1);
+    if (sH.get(s[rt]) === 0) {
+      sH.delete(s[rt]);
+    }
+  }
+
+  let lt = 0;
+  for (let rt = len; rt < s.length; rt++) {
+    sH.set(s[rt], (sH.get[rt] || 0) + 1);
+    if (sH.get(s[rt]) === 0) sH.delete(s[rt]);
+    if (sH.size === 0) answer++;
+    sH.set(s[lt], (sH.get(s[lt]) || 0) - 1);
+    if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
+    lt++;
+  }
 }
 
-let a = 'bacaAacba';
-let b = 'abc';
+let a = "bacaAacba";
+let b = "abc";
 console.log(solution(a, b));
