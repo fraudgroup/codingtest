b25a8e177b8ea4c1b1d4f710d0abccd14b6ea105
김보경 <rlaqhrud6263@naver.com>
2024-09-13 09:07:55 +0900

    Feat: 06-01 풀이

--- 06. 자료구조(스택, 큐)/01. 올바른 괄호/김보경/index.js
+++ 06. 자료구조(스택, 큐)/01. 올바른 괄호/김보경/index.js
@@ -1,8 +1,10 @@
 function solution(s) {
-  let answer = 'YES';
+  let result = [...s]
+    .map((el) => (el === "(" ? 1 : -1))
+    .reduce((acc, el) => acc + el, 0);
 
-  return answer;
+  return result === 0 ? "YES" : "NO";
 }
 
-let a = '(()(()))(()';
+let a = "(()(()))(()";
 console.log(solution(a));
