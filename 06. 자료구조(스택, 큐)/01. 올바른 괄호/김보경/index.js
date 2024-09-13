97bb1c30500c8cb5f8c226574fdfdcc97a5f7f0c
김보경 <rlaqhrud6263@naver.com>
2024-09-13 09:17:52 +0900

    Feat: 06-01 다른 풀이 추가

--- 06. 자료구조(스택, 큐)/01. 올바른 괄호/김보경/index.js
+++ 06. 자료구조(스택, 큐)/01. 올바른 괄호/김보경/index.js
@@ -6,5 +6,23 @@ function solution(s) {
   return result === 0 ? "YES" : "NO";
 }
 
+// stack 사용 풀이 추가
+const solution2 = (s) => {
+  let answer = "YES";
+  let stack = [];
+
+  [...s].forEach((el) => {
+    if (el === "(") {
+      stack.push(el);
+    } else {
+      if (stack.length === 0) return "NO";
+      stack.pop();
+    }
+  });
+
+  return stack.length === 0 ? answer : "NO";
+};
+
 let a = "(()(()))(()";
 console.log(solution(a));
+console.log(solution2(a));
