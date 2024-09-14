7e2ab9015463ce3c2337b1587e3d700dc7dc538e
minyoung <alsdyd1021@naver.com>
2024-09-15 00:49:21 +0900

    [민영] 3-2. 유효햔 팰린드롬 수정

--- 03. 문자열 탐색/02. 유효한 팰린드롬/김민영/index.js
+++ 03. 문자열 탐색/02. 유효한 팰린드롬/김민영/index.js
@@ -1,11 +1,9 @@
 function solution(s) {
   const cleanedString = s
-    .replace(/[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g, '')
-    .replace(/[0-9]/g, '')
-    .replace(/\s+/g, '')
-    .toLowerCase();
+    .toLowerCase()
+    .replace(/[^a-z]/g, '')
 
-  return cleanedString === cleanedString.split('').reverse().join('') ? 'YES' : 'NO';
+  return cleanedString === [...cleanedString].reverse().join('') ? 'YES' : 'NO';
 }
 
 let str = 'found7, time: study; Yduts; emit, 7Dnuof';
