ede549fa35246ddc80fdd90dfb724dde620c0f94
seolsis <seoljw1005@naver.com>
2024-09-14 14:54:05 +0900

    3-2. 유효한 팰린드롬

--- 03. 문자열 탐색/02. 유효한 팰린드롬/백지원/index.js
+++ 03. 문자열 탐색/02. 유효한 팰린드롬/백지원/index.js
@@ -1,8 +1,15 @@
 function solution(s) {
-  let answer = 'YES';
+  let answer = "YES";
+  // 알파벳 이외의 문자는 무시
+  s = s.toLowerCase().replace(/[^a-z]/g, "");
+  let n = s.length;
 
+  for (let i = 0; i < n; i++) {
+    if (s[i] !== s[n - i - 1]) answer = "NO";
+    break;
+  }
   return answer;
 }
 
-let str = 'found7, time: study; Yduts; emit, 7Dnuof';
+let str = "found7, time: study; Yduts; emit, 7Dnuof";
 console.log(solution(str));
