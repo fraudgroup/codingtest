7b0b1625565d13c28b3b398d5bbaf4eaabd439e6
Seoin <seoin2269@naver.com>
2024-09-16 23:46:53 +0900

    feat: 9-6 풀이

--- 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/06. 송아지 찾기(BFS)/이서인/index.js
+++ 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/06. 송아지 찾기(BFS)/이서인/index.js
@@ -1,7 +1,23 @@
 function solution(s, e) {
-  let answer = 0;
+  const directions = [1, -1, 5];
+  const visited = new Array(10001).fill(false);
+  const queue = [];
 
-  return answer;
+  queue.push([s, 0]);
+  visited[s] = true;
+
+  while (queue.length) {
+    const [current, jumps] = queue.shift();
+    if (current === e) return jumps;
+
+    for (let direction of directions) {
+      const next = current + direction;
+      if (next >= 0 && next <= 10000 && !visited[next]) {
+        visited[next] = true;
+        queue.push([next, jumps + 1]);
+      }
+    }
+  }
 }
 
-console.log(solution(8, 3));
+console.log(solution(5, 14));
