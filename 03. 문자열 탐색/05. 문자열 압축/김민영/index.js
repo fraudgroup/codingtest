1f19fb891d01aec9ae14b45927e44e7eacf185dc
minyoung <alsdyd1021@naver.com>
2024-09-20 15:51:51 +0900

    [민영] 3-5. 문자열 압축

--- 03. 문자열 탐색/05. 문자열 압축/김민영/index.js
+++ 03. 문자열 탐색/05. 문자열 압축/김민영/index.js
@@ -1,5 +1,18 @@
 function solution(s) {
   let answer = '';
+  let cnt = 1;
+
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      cnt++;
+    } else {
+      if (cnt > 1) {
+        answer += cnt;
+      }
+      answer += s[i];
+      cnt = 1;
+    }
+  }
 
   return answer;
 }
