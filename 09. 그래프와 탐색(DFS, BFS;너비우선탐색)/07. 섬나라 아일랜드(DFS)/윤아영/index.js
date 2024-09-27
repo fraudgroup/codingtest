e10e7e02f7e7fdf6893302fb7894be441221718a
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-28 00:48:47 +0900

    feat: 섬나라 아일랜드 DFS 풀이

--- 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/07. 섬나라 아일랜드(DFS)/윤아영/index.js
+++ 09. 그래프와 탐색(DFS, BFS;너비우선탐색)/07. 섬나라 아일랜드(DFS)/윤아영/index.js
@@ -1,5 +1,30 @@
 function solution(board) {
   let answer = 0;
+  const dx = [-1, 0, 1, -1, 1, -1, 0, 1];
+  const dy = [-1, -1, -1, 0, 0, 1, 1, 1];
+
+  const DFS = (x, y) => {
+    board[x][y] = 0;
+    for (let i = 0; i < dx.length; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+      if (nx < 0 || nx >= board.length || ny < 0 || ny >= board.length || board[nx][ny] !== 1) {
+        continue;
+      }
+
+      DFS(x + dx[i], y + dy[i]);
+    }
+  };
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
 
   return answer;
 }
