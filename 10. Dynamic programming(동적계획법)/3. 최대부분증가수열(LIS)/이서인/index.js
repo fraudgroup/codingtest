91780af302cc7145ffb978a91a6a57ba4844d111
Seoin <seoin2269@naver.com>
2024-09-28 01:03:37 +0900

    feat: 10-3 풀이

--- 10. Dynamic programming(동적계획법)/3. 최대부분증가수열(LIS)/이서인/index.js
+++ 10. Dynamic programming(동적계획법)/3. 최대부분증가수열(LIS)/이서인/index.js
@@ -1,6 +1,16 @@
 function solution(arr) {
+  let dp = Array(arr.length).fill(1);
   let answer = 0;
 
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    answer = Math.max(answer, dp[i])
+  }
+  
   return answer;
 }
 
