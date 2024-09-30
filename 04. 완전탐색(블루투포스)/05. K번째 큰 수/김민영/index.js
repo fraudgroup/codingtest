2aef1d844c37b586a735ecdc6faf6bd761e04a6e
minyoung <alsdyd1021@naver.com>
2024-09-30 15:10:31 +0900

    [민영] 4-5. K번째 큰 수

--- 04. 완전탐색(블루투포스)/05. K번째 큰 수/김민영/index.js
+++ 04. 완전탐색(블루투포스)/05. K번째 큰 수/김민영/index.js
@@ -1,7 +1,18 @@
 function solution(n, k, card) {
   let answer;
+  let arr = new Set;
 
-  return answer;
+  for (i = 0; i < n; i++) {
+    for (j = i + 1; j < n; j++) {
+      for (x = j + 1; x < n; x++) {
+        arr.add(card[i] + card[j] + card[x]);
+      }
+    }
+  }
+
+  answer = Array.from(arr).sort((a, b) => b - a);
+
+  return answer[k - 1];
 }
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
