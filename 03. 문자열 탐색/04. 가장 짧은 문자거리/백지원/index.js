e9d15be3e4e415c26d154c76d1bce252c1936f7c
seolsis <seoljw1005@naver.com>
2024-10-04 13:43:47 +0900

    3-4. 가장 짧은 문자거리

--- 03. 문자열 탐색/04. 가장 짧은 문자거리/백지원/index.js
+++ 03. 문자열 탐색/04. 가장 짧은 문자거리/백지원/index.js
@@ -1,8 +1,22 @@
 function solution(s, t) {
   let answer = [];
+  let n = s.length;
+  let p = 0;
 
-  return answer;
+  for (let i = 0; i < n; i++) {
+    if (s[i] === t) p = 0;
+    else p++;
+    answer.push(p);
+  }
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (s[i] === t) p = 0;
+    else p++;
+    answer[i] = Math.min(answer[i], p);
+  }
+
+  return answer.join(" ");
 }
 
-let str = 'teachermode';
-console.log(solution(str, 'e'));
+let str = "teachermode";
+console.log(solution(str, "e"));
