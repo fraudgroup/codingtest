c626c8d8405eb18d6e07acf8f44e89e453befffe
seolsis <seoljw1005@naver.com>
2024-10-04 14:55:35 +0900

    4-1. 자리수의 합

--- 04. 완전탐색(블루투포스)/01. 자리수의 합/백지원/index.js
+++ 04. 완전탐색(블루투포스)/01. 자리수의 합/백지원/index.js
@@ -1,6 +1,21 @@
-function solution(n, arr) {
-  let answer;
+function digitSum(num) {
+  return num
+    .toString()
+    .split("")
+    .reduce((acc, curr) => acc + parseInt(curr), 0);
+}
 
+function solution(n, arr) {
+  let answer = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < n; i++) {
+    let sum = digitSum(arr[i]);
+    if (sum > maxSum || (sum === maxSum && arr[i] > answer)) {
+      maxSum = sum;
+      answer = arr[i];
+    }
+  }
   return answer;
 }
 
