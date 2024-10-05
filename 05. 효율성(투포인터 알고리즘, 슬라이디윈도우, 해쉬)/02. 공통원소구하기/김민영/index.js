6af83f205d13757a19a4756d0a592f21906a8371
minyoung <alsdyd1021@naver.com>
2024-10-06 01:13:29 +0900

    [민영] 5-2. 공통원소구하기

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/김민영/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/김민영/index.js
@@ -1,5 +1,24 @@
 function solution(arr1, arr2) {
   let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = 0;
+  let p2 = 0;
+
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] !== arr2[p2]) {
+      arr1[p1] < arr2[p2] ? p1++ : p2++;
+    } else {
+      answer.push(arr1[p1]);
+      p1++;
+      p2++;
+    }
+  }
+
+
 
   return answer;
 }
