c68955c1ade76b70dca338a9dbb816d5a7993912
yu kyoung kim <sksms2104@naver.com>
2024-10-12 17:49:09 +0900

    4-2: 불필요한 변수 선언없이 해결

--- 04. 완전탐색(블루투포스)/02. 뒤집은 소수/김유경/index.js
+++ 04. 완전탐색(블루투포스)/02. 뒤집은 소수/김유경/index.js
@@ -1,30 +1,17 @@
 function solution(arr) {
-  const answer = [];
-
-  const reverseNumber = (n) => {
-    return Number(n.toString().split("").reverse().join(""));
-  };
+  const reverseNumber = (n) =>
+    Number(n.toString().split("").reverse().join(""));
 
   const checkIsDecimal = (n) => {
-    // 2보다 작으면 소수가 아님
     if (n < 2) return false;
-
-    // 소수인지 아닌지 반복문 태우기
     for (let i = 2; i < n; i++) {
-      // 나머지가 0, 즉 나눠지는 숫자가 있다는 것은 소수가 아니라는 것
       if (n % i === 0) return false;
     }
     return true;
   };
 
-  const reverseArray = arr.map((n) => reverseNumber(n));
-  reverseArray.map((n) => {
-    if (checkIsDecimal(n)) {
-      answer.push(n);
-    }
-  });
-
-  return answer;
+  // 뒤집고, isDecimal일 것만 남겨서 return
+  return arr.map(reverseNumber).filter(checkIsDecimal);
 }
 
 let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
