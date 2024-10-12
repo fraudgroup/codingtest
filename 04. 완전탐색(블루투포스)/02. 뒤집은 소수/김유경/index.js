561c175bfc26d8c3f8c881903cd878718ed5bbce
yu kyoung kim <sksms2104@naver.com>
2024-10-12 17:43:36 +0900

    4-2: 뒤집은 소수

--- 04. 완전탐색(블루투포스)/02. 뒤집은 소수/김유경/index.js
+++ 04. 완전탐색(블루투포스)/02. 뒤집은 소수/김유경/index.js
@@ -1,5 +1,28 @@
 function solution(arr) {
-  let answer = [];
+  const answer = [];
+
+  const reverseNumber = (n) => {
+    return Number(n.toString().split("").reverse().join(""));
+  };
+
+  const checkIsDecimal = (n) => {
+    // 2보다 작으면 소수가 아님
+    if (n < 2) return false;
+
+    // 소수인지 아닌지 반복문 태우기
+    for (let i = 2; i < n; i++) {
+      // 나머지가 0, 즉 나눠지는 숫자가 있다는 것은 소수가 아니라는 것
+      if (n % i === 0) return false;
+    }
+    return true;
+  };
+
+  const reverseArray = arr.map((n) => reverseNumber(n));
+  reverseArray.map((n) => {
+    if (checkIsDecimal(n)) {
+      answer.push(n);
+    }
+  });
 
   return answer;
 }
