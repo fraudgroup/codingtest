52e63c2af27d2403cec7b02571b3b860b5b31f27
yu kyoung kim <sksms2104@naver.com>
2024-10-14 21:04:31 +0900

    4-4: 졸업선물 문제풀이

--- 04. 완전탐색(블루투포스)/04. 졸업선물/김유경/index.js
+++ 04. 완전탐색(블루투포스)/04. 졸업선물/김유경/index.js
@@ -1,7 +1,26 @@
 function solution(m, product) {
-  let answer = 0;
+  let count = 0; // 구매할 수 있는 학생 수
+  let price = 0; // 사용하는 금액
 
-  return answer;
+  // 배송비 + 할인된 물품 금액
+  const totalPrice = (a, b) => {
+    return a / 2 + b;
+  };
+
+  // 실제 사용할 금액으로 바꾼 뒤 오름차순 정렬하기
+  const array = product
+    .map((n) => totalPrice(n[0], n[1]))
+    .sort((a, b) => a - b);
+
+  // 앞에서부터 더해서, 예산을 넘었을 때는 count 하지 않은 채로 이전 count 값까지 return
+  for (let i = 0; i < array.length; i++) {
+    price += array[i];
+
+    if (price < m) {
+      count++;
+    }
+  }
+  return count;
 }
 
 let arr = [
