5a5ce9752d06729c88d815310d1e848a3305e76d
minyoung <alsdyd1021@naver.com>
2024-10-14 16:42:42 +0900

    [민영] 5-6. 학습 회장(해쉬)

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/06. 학급 회장(해쉬)/김민영/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/06. 학급 회장(해쉬)/김민영/index.js
@@ -1,7 +1,27 @@
 function solution(s) {
-  let answer;
+  let votes = new Map([
+    ['A', 0],
+    ['B', 0],
+    ['C', 0],
+    ['D', 0],
+    ['E', 0]
+  ]);
 
-  return answer;
+  for (x of s) {
+    votes.set(x, votes.get(x) + 1);
+  }
+
+  let maxVotes = 0;
+  let winner = '';
+
+  for (let [key, value] of votes) {
+    if (value > maxVotes) {
+      maxVotes = value;
+      winner = key;
+    }
+  }
+
+  return winner;
 }
 
 let str = 'BACBACCACCBDEDE';
