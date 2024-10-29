86d3d972e9e86d13c9d098ca5e0eec791c4c04a7
seolsis <seoljw1005@naver.com>
2024-10-29 23:39:51 +0900

    5-2. 공통원소구하기

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/백지원/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/02. 공통원소구하기/백지원/index.js
@@ -1,5 +1,21 @@
 function solution(arr1, arr2) {
   let answer = [];
+  let p1 = 0,
+    p2 = 0;
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1]);
+      p1++;
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) {
+      p1++;
+    } else {
+      p2++;
+    }
+  }
 
   return answer;
 }
