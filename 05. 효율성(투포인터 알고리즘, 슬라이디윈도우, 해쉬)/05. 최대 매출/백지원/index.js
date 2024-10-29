66cc7a229af2a310675234399a6ef90be8ee6254
seolsis <seoljw1005@naver.com>
2024-10-30 00:30:31 +0900

    5-5. 최대 매출

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/05. 최대 매출/백지원/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/05. 최대 매출/백지원/index.js
@@ -1,6 +1,16 @@
 function solution(k, arr) {
-  let answer;
+  let answer = 0;
+  let sum = 0;
 
+  for (let i = 0; i < k; i++) {
+    sum += arr[i];
+  }
+  answer = sum;
+
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    answer = Math.max(answer, sum);
+  }
   return answer;
 }
 
