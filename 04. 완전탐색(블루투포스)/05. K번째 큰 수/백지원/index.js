5704a990d600410a5a6d0d522c3f7878ff7d7aa2
seolsis <seoljw1005@naver.com>
2024-10-29 20:18:40 +0900

    4-5. K번째 큰 수

--- 04. 완전탐색(블루투포스)/05. K번째 큰 수/백지원/index.js
+++ 04. 완전탐색(블루투포스)/05. K번째 큰 수/백지원/index.js
@@ -1,7 +1,14 @@
 function solution(n, k, card) {
-  let answer;
-
-  return answer;
+  let s = new Set();
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let l = j + 1; l < n; l++) {
+        s.add(arr[i] + arr[j] + arr[l]);
+      }
+    }
+  }
+  // s 배열로 변환
+  return [...s].sort((a, b) => b - a)[k - 1];
 }
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
