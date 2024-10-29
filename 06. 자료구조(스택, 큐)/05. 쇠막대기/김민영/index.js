8bd4467b56090b286f8d34534a9b78a325e9b383
minyoung <alsdyd1021@naver.com>
2024-10-29 22:56:43 +0900

    [민영] 6-5. 쇠막대기

--- 06. 자료구조(스택, 큐)/05. 쇠막대기/김민영/index.js
+++ 06. 자료구조(스택, 큐)/05. 쇠막대기/김민영/index.js
@@ -1,8 +1,26 @@
 function solution(s) {
   let answer = 0;
+  let stack = [];
+
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      stack.push('(');
+    } else {
+      stack.pop();
+
+      if (s[i - 1] === '(') {
+        answer += stack.length;
+      } else {
+        answer += 1;
+      }
+    }
+  }
 
   return answer;
 }
 
 let a = '()(((()())(())()))(())';
 console.log(solution(a));
+
+let b = '(((()(()()))(())()))(()())';
+console.log(solution(b));
