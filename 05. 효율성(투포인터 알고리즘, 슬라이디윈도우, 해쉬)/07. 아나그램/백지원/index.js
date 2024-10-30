38db60fc03003f99a393da39a1d5749d4927c3cf
seolsis <seoljw1005@naver.com>
2024-10-30 14:35:50 +0900

    5-7. 아나그램

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/07. 아나그램/백지원/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/07. 아나그램/백지원/index.js
@@ -1,9 +1,23 @@
 function solution(str1, str2) {
-  let answer = 'YES';
+  let answer = "YES";
+  let alpha1 = new Map();
+  for (let alpha of str1) {
+    if (alpha1.has(alpha)) {
+      alpha1.set(alpha, alpha1.get(alpha) + 1);
+    } else {
+      alpha1.set(alpha, 1);
+    }
+  }
 
+  for (let alpha of str2) {
+    if (!alpha1.has(alpha) || alpha1.get(alpha) === 0) {
+      return "NO";
+    }
+    alpha1.set(alpha, alpha1.get(alpha) - 1);
+  }
   return answer;
 }
 
-let a = 'AbaAeCe';
-let b = 'baeeACA';
+let a = "AbaAeCe";
+let b = "baeeACA";
 console.log(solution(a, b));
