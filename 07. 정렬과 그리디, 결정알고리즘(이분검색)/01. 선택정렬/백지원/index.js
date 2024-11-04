5c8b8ee71e05b480702b6215917b3dafce10d148
seolsis <seoljw1005@naver.com>
2024-11-04 22:35:33 +0900

    7-1. 선택정렬

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/01. 선택정렬/백지원/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/01. 선택정렬/백지원/index.js
@@ -1,6 +1,14 @@
 function solution(arr) {
   let answer = arr;
-
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
   return answer;
 }
 
