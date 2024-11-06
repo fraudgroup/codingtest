da53044da40e0b6960e73e9be6331536af7cc568
seolsis <seoljw1005@naver.com>
2024-11-06 15:23:41 +0900

    7-7. 좌표정렬

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬/백지원/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/07. 좌표 정렬/백지원/index.js
@@ -1,7 +1,11 @@
 function solution(arr) {
-  let answer = arr;
-
-  return answer;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  });
+  return arr;
 }
 
 let arr = [
