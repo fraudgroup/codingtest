8e29756d1d628425723cc02a7f44a8bd0acfb654
seolsis <seoljw1005@naver.com>
2024-11-20 22:47:50 +0900

    8-2. 이진수 출력

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/02. 이진수 출력(재귀)/백지원/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/02. 이진수 출력(재귀)/백지원/index.js
@@ -1,7 +1,13 @@
 function solution(n) {
-  let answer = '';
+  let answer = [];
 
-  return answer;
+  function DFS(n) {
+    if (n === 0) return;
+    DFS(Math.floor(n / 2));
+    answer.push(n % 2);
+  }
+  DFS(n);
+  return answer.join("");
 }
 
 console.log(solution(11));
