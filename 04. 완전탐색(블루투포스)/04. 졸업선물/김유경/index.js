1aef7964bb7fd9482a52cc86c2e096bcf0603ff8
codingaring <sksms2104@naver.com>
2024-11-23 22:31:11 +0900

    yukyoung_4-4: 문제 이해 후 로직 수정 및 테스트 케이스 추가

--- 04. 완전탐색(블루투포스)/04. 졸업선물/김유경/index.js
+++ 04. 완전탐색(블루투포스)/04. 졸업선물/김유경/index.js
@@ -3,13 +3,25 @@ function solution(m, product) {
   let price = 0; // 사용하는 금액
 
   // 배송비 + 할인된 물품 금액
-  const totalPrice = (a, b) => {
+  const salePrice = (a, b) => {
     return a / 2 + b;
   };
 
-  // 실제 사용할 금액으로 바꾼 뒤 오름차순 정렬하기
+  // 물품 금액 합
+  const totalPrice = (a, b) => {
+    return a + b;
+  };
+
+  // 상품 가격 비싼놈 맨 앞에 세워서 반값 쿠폰 쓰고, 나머지는 물품 값 합계 구한 다음 오름차순으로 정렬
   const array = product
-    .map((n) => totalPrice(n[0], n[1]))
+    .sort((a, b) => b[0] - a[0])
+    .map((n, index) => {
+      if (index === 0) {
+        return salePrice(n[0], n[1]);
+      }
+
+      return totalPrice(n[0], n[1]);
+    })
     .sort((a, b) => a - b);
 
   // 앞에서부터 더해서, 예산을 넘었을 때는 count 하지 않은 채로 이전 count 값까지 return
@@ -21,11 +33,44 @@ function solution(m, product) {
   return count;
 }
 
-let arr = [
-  [6, 6],
-  [2, 2],
-  [4, 3],
-  [4, 5],
-  [10, 3],
-];
-console.log(solution(28, arr));
+console.log(
+  solution(28, [
+    [6, 6],
+    [2, 2],
+    [4, 3],
+    [4, 5],
+    [10, 3],
+  ])
+);
+// 답 : 4
+
+console.log(
+  solution(41, [
+    [8, 6],
+    [2, 2],
+    [4, 3],
+    [4, 5],
+    [12, 1],
+  ])
+);
+// 답 : 5
+
+console.log(
+  solution(41, [
+    [8, 6],
+    [2, 2],
+    [12, 1],
+    [4, 5],
+    [4, 3],
+  ])
+);
+// 답 : 5
+
+console.log(
+  solution(33, [
+    [2, 12],
+    [8, 4],
+    [6, 6],
+    [6, 7],
+  ])
+); // 답 : 3
