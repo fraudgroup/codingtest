a35e5062843b38450083ce86829c0c5551bb35a5
minyoung <alsdyd1021@naver.com>
2024-11-30 21:57:36 +0900

    [민영] 7-9. 결혼식

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/09. 결혼식/김민영/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/09. 결혼식/김민영/index.js
@@ -1,7 +1,25 @@
 function solution(times) {
-  let answer = Number.MIN_SAFE_INTEGER;
+  let events = [];
 
-  return answer;
+  for (let [start, end] of times) {
+    events.push([start, 1]);
+    events.push([end, -1]);
+  }
+
+  events.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    return a[0] - b[0];
+  });
+
+  let current = 0;
+  let maxPeople = 0;
+
+  for (let [time, type] of events) {
+    current += type;
+    maxPeople = Math.max(maxPeople, current);
+  }
+
+  return maxPeople;
 }
 
 let arr = [
