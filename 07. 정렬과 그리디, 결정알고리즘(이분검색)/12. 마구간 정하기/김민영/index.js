bd8caddf9def837cf9c236f85c9685c9a5808542
minyoung <alsdyd1021@naver.com>
2024-12-10 13:22:11 +0900

    [민영] 7-12. 마구간 정하기

--- 07. 정렬과 그리디, 결정알고리즘(이분검색)/12. 마구간 정하기/김민영/index.js
+++ 07. 정렬과 그리디, 결정알고리즘(이분검색)/12. 마구간 정하기/김민영/index.js
@@ -1,5 +1,36 @@
 function solution(c, stable) {
-  let answer;
+  stable.sort((a, b) => a - b);
+
+  let start = 1;
+  let end = stable[stable.length - 1] - stable[0];
+  let answer = 0;
+
+  const canPlaceHorses = (distance) => {
+    let count = 1;
+    let lastPosition = stable[0];
+
+    for (i = 1; i < stable.length; i++) {
+      if (stable[i] - lastPosition >= distance) {
+        count++;
+        lastPosition = stable[i];
+
+        if (count >= c) return true;
+      }
+    }
+
+    return false;
+  };
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (canPlaceHorses(mid)) {
+      answer = mid;
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
 
   return answer;
 }
