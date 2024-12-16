da806b476b8d69ded6df8133f2b4d345602bf008
codingaring <sksms2104@naver.com>
2024-12-16 16:47:57 +0900

    4-5 문제풀이

--- 04. 완전탐색(블루투포스)/05. K번째 큰 수/김유경/index.js
+++ 04. 완전탐색(블루투포스)/05. K번째 큰 수/김유경/index.js
@@ -1,7 +1,22 @@
-function solution(n, k, card) {
-  let answer;
+function solution(n, k, cards) {
+ let sums = []
+ 
+ function dfs(start, depth, sum){
+  if(depth === k){
+    sums.push(sum);
+    return;
+  }
 
-  return answer;
+  for(let i = start; i < n; i ++){
+    dfs(i + 1, depth + 1, sum + cards[i])
+  }
+ }
+
+ dfs(0,0,0)
+
+ const sortedUniqueSums = [...new Set(sums)].sort((a, b)=> b-a)
+
+ return sortedUniqueSums[k] || -1
 }
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
