f4044635c7675f6e446bdfbc5b80e32a92e06a52
minyoung <alsdyd1021@naver.com>
2025-01-31 17:06:25 +0900

    [민영] 8-3. 이진트리순회

--- 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/03. 이진트리순회/김민영/index.js
+++ 08. 재귀함수와 완전탐색(DFS;깊이우선탐색)/03. 이진트리순회/김민영/index.js
@@ -1,7 +1,45 @@
-function solution(n) {
+function preorderTraversal(n) {
   let answer = '';
-
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      answer += v + ' ';
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+    }
+  }
+  DFS(n);
   return answer;
 }
 
-console.log(solution(1));
+function inorderTraversal(n) {
+  let answer = '';
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      DFS(v * 2);
+      answer += v + ' ';
+      DFS(v * 2 + 1);
+    }
+  }
+  DFS(n);
+  return answer;
+}
+
+function postorderTraversal(n) {
+  let answer = '';
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+      answer += v + ' ';
+    }
+  }
+  DFS(n);
+  return answer;
+}
+
+console.log("전위순회 출력:", preorderTraversal(1));
+console.log("중위순회 출력:", inorderTraversal(1));
+console.log("후위순회 출력:", postorderTraversal(1));
